937b4597d489cf58eaf2eb917cea89ea08459898
Lucas Holmquist <lholmqui@redhat.com>
2013-06-25 14:55:22 -0400

    adding in mockjax

---
+++ app/bower_components/jquery-mockjax/CHANGELOG.md
@@ -0,0 +1,19 @@
+## 2012-05-30
+	* Updated to version 1.5.2
+	* Added support for jQuery 1.8, 1.9, 2.0
+	* TODO: Update this file with all changes since previous version
+
+## 2011-03-25   Jonathan Sharp (http://jdsharp.com)
+    * Updating jQuery 1.5rc1 to 1.5.1
+	* Adding TestSwarm support
+
+## 2011-02-03	Jonathan Sharp (http://jdsharp.com)
+	* Added log setting to intercept or disable logging messages
+	* Added proxyType setting to force request type when proxying a mock
+	* Added 29 unit tests for jQuery 1.3 through 1.5
+	* Fixed issue #4 - Compatibility with jQuery 1.3
+	* Fixed issue #10 - Undefined contents
+	* Fixed issue #15 - proxy setting request type
+	* Fixed issue #16 - proxy setting request type
+	* Fixed issue #17 - jsonp request handling
+	* Fixed issue #18 - Unit test fail with jQuery 1.5
---
+++ app/scripts/mocks/mock.js
@@ -0,0 +1,66 @@
+var apps = [
+    {
+        "id": "402881893f62a12c013f631dc6cd0000",
+        "name": "Application 1",
+        "description": "Application 1 Description",
+        "pushApplicationID": "8a0cf1df-0bcc-4a75-8b2a-78dff2915038",
+        "masterSecret": "a04f822c-2224-45d9-a203-297e37c26c93",
+        "developer": "admin",
+        "androidApps": [{
+            "id": "402881893f62a12c013f66f676c80001",
+            "name": "Application1Android",
+            "description": "An Android Variant of the Mobile App",
+            "variantID": "32cede3c-27df-4eac-b807-65c31fbfeabc",
+            "secret": "66063664-4a87-4b02-8c0b-1d2b0cec0726",
+            "developer": "admin",
+            "instances": [],
+            "googleKey": "1234567890"
+        }],
+        "simplePushApps": [],
+        "iosapps": []
+    }
+];
+
+var baseURL = "ag-push/rest/";
+
+$.mockjax({
+    type: "GET",
+    url: baseURL + "applications",
+    responseTime: 750,
+    dataType: "json",
+    response: function() {
+        this.responseText = apps;
+    }
+});
+
+
+$.mockjax({
+    type: "POST",
+    url: baseURL + "applications",
+    responseTime: 750,
+    dataType: "json",
+    response: function() {
+        apps.push( {
+            "id": "402881893f62a12c013f631dc6cd0000",
+            "name": "Application 1",
+            "description": "Application 1 Description",
+            "pushApplicationID": "8a0cf1df-0bcc-4a75-8b2a-78dff2915038",
+            "masterSecret": "a04f822c-2224-45d9-a203-297e37c26c93",
+            "developer": "admin",
+            "androidApps": [{
+                "id": "402881893f62a12c013f66f676c80001",
+                "name": "Application1Android",
+                "description": "An Android Variant of the Mobile App",
+                "variantID": "32cede3c-27df-4eac-b807-65c31fbfeabc",
+                "secret": "66063664-4a87-4b02-8c0b-1d2b0cec0726",
+                "developer": "admin",
+                "instances": [],
+                "googleKey": "1234567890"
+            }],
+            "simplePushApps": [],
+            "iosapps": []
+        });
+        this.responseText = apps;
+    }
+});
+
