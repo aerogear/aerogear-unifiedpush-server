7afd246ae1441bdc5bb5cb9af3f97ac6dc7bbe08
Lucas Holmquist <lholmqui@redhat.com>
2013-06-25 14:55:41 -0400

    more better ember

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,43 +1,4 @@
 /*var adminApp = {
-    init: function() {
-
-        //this.aerogear.applicationsStore = AeroGear.DataManager( "applicationsStore" ).stores.applicationsStore;
-
-        //Setup event handlers
-        console.log( "initialized" );
-    },
-    logout: function() {
-        this.aerogear.authenticator.logout({
-            contentType: "application/json",
-            success: function( success ) {
-                console.log( "Logged Out", success );
-                adminApp.forceLogin();
-            },
-            error: function( error ) {
-                console.log( "Error Logging Out", error );
-            }
-        });
-    },
-    getMobileApplication: function( applicationId ) {
-        var applicationPipe = this.aerogear.pipelines.pipes.applications;
-
-        applicationPipe.read({
-            id: applicationId,
-            success: function( response ) {
-                console.log( "application reponse", response );
-            },
-            error: function( error ) {
-                console.log( "error with application endpoint", error );
-                switch( error.status ) {
-                case 401:
-                    adminApp.forceLogin();
-                    break;
-                default:
-                    break;
-                }
-            }
-        });
-    },
     saveMobileApplication: function( applicationData ) {
         var applicationPipe = this.aerogear.pipelines.pipes.applications;
 
@@ -110,28 +71,44 @@ var App = Ember.Application.create({
     LOG_TRANSITIONS: true
 });
 
-
 /*
-Router
+    Application Router
+
+    Use resource for nested routes
 */
 App.Router.map( function() {
+    // The Login Page
     this.route( "login" );
-    this.route( "applications" );
+    // The Main List of Mobile Applications and the main starting point.  This is a nested route since the header/footer will be similar.
+    this.resource( "mobileApps", function() {
+        this.resource( "appcreate" );
+        this.resource( "app", { path: "app/:pushApplicationID" }, function(){
+            this.resource( "variants" );
+        });
+    });
+    // The Route for a single Mobile Application.
+    /*this.resource( "app", { path: "app/:pushApplicationId"}, function() {
+        this.resource( "variants" );
+        this.resource( "instances" );
+    });*/
 });
 
+/*
+    Application Index Route
+*/
 App.IndexRoute = Ember.Route.extend({
     redirect: function() {
-        this.transitionTo( "applications" );
+        this.transitionTo( "mobileApps" );
     }
 });
 
 /*
-An Object Representing a list of Mobile Apps
+    Ember Application Controller.
 */
-App.MobileApplications = Ember.Object.extend({});
+//App.ApplicationController = Ember.Controller.extend({});
 
 /*
-Login Route
+    Login Route
 */
 App.LoginRoute = Ember.Route.extend({
     events: {
@@ -142,10 +119,11 @@ App.LoginRoute = Ember.Route.extend({
             App.AeroGear.authenticator.login( JSON.stringify( data ), {
                 contentType: "application/json",
                 success: function( success ) {
-                    that.transitionTo( "applications" );
+                    that.transitionTo( "mobileApps" );
                     console.log( "Logged in", success );
                 },
                 error: function( error ) {
+                    that.transitionTo( "mobileApps" );
                     console.log( "Error Logging in", error );
                 }
             });
@@ -154,11 +132,11 @@ App.LoginRoute = Ember.Route.extend({
 });
 
 /*
-Mobile Applications Route
+    Mobile Applications Index Route
 */
-App.ApplicationsRoute = Ember.Route.extend({
+App.MobileAppsIndexRoute = Ember.Route.extend({
     model: function() {
-        return App.MobileApplications.create( { apps: [] } );
+        return App.mobileApplicationsInstance;
     },
     setupController: function( controller, model ) {
         model.setProperties(this.getMobileApplication( model ));
@@ -171,7 +149,7 @@ App.ApplicationsRoute = Ember.Route.extend({
             id: applicationPushId,
             success: function( response ) {
                 console.log( "application reponse", response );
-                model.setProperties( { apps: response } );
+                model.setProperties( { apps: response, isLoaded: true } );
             },
             error: function( error ) {
                 console.log( "error with application endpoint", error );
@@ -180,13 +158,91 @@ App.ApplicationsRoute = Ember.Route.extend({
                     that.transitionTo( "login" );
                     break;
                 default:
-                    that.transitionTo( "login" );
+                    //that.transitionTo( "login" );
+                    model.setProperties( { isLoaded: true, error: error } );
                     break;
                 }
             }
         });
     }
 });
+
+/*
+    The Mobile Apps Controller. Put "Global Events" Here
+*/
+App.MobileAppsController = Ember.Controller.extend({
+    logout: function() {
+        var that = this;
+        App.AeroGear.authenticator.logout({
+            contentType: "application/json",
+            success: function( success ) {
+                console.log( "Logged Out", success );
+                that.transitionTo( "login" );
+            },
+            error: function( error ) {
+                console.log( "Error Logging Out", error );
+                that.transitionTo( "login" );
+            }
+        });
+    }
+});
+
+/*
+    The Controller for adding new Mobile apps
+*/
+App.AppcreateController = Ember.Controller.extend({
+    add: function() {
+        this.saveMobileApplication();
+    },
+    saveMobileApplication: function( applicationData ) {
+        var applicationPipe = App.AeroGear.pipelines.pipes.applications,
+            that = this;
+
+        applicationPipe.save( applicationData, {
+            success: function( response ) {
+                console.log( response );
+                that.transitionToRoute( "mobileApps" );
+            },
+            error: function( error ) {
+                console.log( "error saving", error );
+                switch( error.status ) {
+                case 401:
+                    that.transitionTo( "login" );
+                    break;
+                default:
+                    break;
+                }
+            }
+        });
+    },
+    cancel: function() {
+        this.transitionToRoute( "mobileApps" );
+        console.log( "cancel" );
+    }
+});
+
+/*App.AppRoute = Ember.Route.extend({
+    model: function() {
+        console.log( this );
+    }
+});*/
+
+//App.AppVariantsRoute = Ember.Route.extend({});
+
+//App.AppNetworksRoute = Ember.Route.extend({});
+
+//App.AppInstancesRoute = Ember.Route.extend({});
+
+/*
+An Object Representing a list of Mobile Apps
+*/
+App.MobileApplications = Ember.Object.extend({ isLoaded: false });
+
+
+/*
+An instance of the object
+*/
+App.mobileApplicationsInstance = App.MobileApplications.create( { apps: [] } );
 
 /*
 AeroGear related things
@@ -205,14 +261,14 @@ App.AeroGear.pipelines = AeroGear.Pipeline([
         name: "applications",
         settings: {
             id: "pushApplicationID",
-            baseURL: "http://localhost:8080/ag-push/rest/",
+            baseURL: "ag-push/rest/",
             authenticator: App.AeroGear.authenticator
         }
     },
     {   //Might not be needed here,  just on device?
         name: "registration",
         settings: {
-            baseURL: "http://localhost:8080/ag-push/rest/",
+            baseURL: "ag-push/rest/",
             authenticator: App.AeroGear.authenticator,
             endpoint: "registry/device"
         }
