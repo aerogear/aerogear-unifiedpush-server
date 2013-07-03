b147d26b281e9d70645f9d71a968da46e34fb466
Lucas Holmquist <lholmqui@redhat.com>
2013-07-03 12:04:10 -0400

    new variants page

--- app/scripts/controllers/variants_controller.js
+++ app/scripts/controllers/variants_controller.js
@@ -1,3 +1,43 @@
+App.VariantsIndexController = Ember.ObjectController.extend({
+    remove: function( variant ) {
+        var things = variant,
+            that = this,
+            mobileVariantPipe = AeroGear.Pipeline({
+            name: "mobileVariant",
+            settings: {
+                baseURL: "/ag-push/rest/applications/",
+                authenticator: App.AeroGear.authenticator,
+                endpoint:  variant.pushApplicationID + "/" + variant.get( "type" )
+            }
+        }).pipes.mobileVariant;
+
+        mobileVariantPipe.remove( variant.variantID, {
+            success: function() {
+                var content = that.get("variantList"),
+                    find;
+
+                find = content.find( function( value ) {
+                    return value.variantID === things.variantID;
+                });
+
+                content.removeObject( find );
+            },
+            error: function( error ) { // TODO: Maybe Make this a class method?
+                console.log( "error with application endpoint", error );
+                switch( error.status ) {
+                case 401:
+                    App.Router.router.transitionToRoute("login");
+                    break;
+                default:
+                    //that.transitionToRoute( "login" );
+                    //result.setProperties( { isLoaded: true, error: error } );
+                    break;
+                }
+            }
+        });
+    }
+});
+
 App.VariantsAddController = Ember.ObjectController.extend({
     add: function() {
         var applicationData = {
