0d6a3830180281d30a734f23fdacab00f463b042
Matthias Wessendorf <matzew@apache.org>
2013-07-16 16:41:10 +0200

    Using HttpUrlConnection instead of AHC

--- src/main/java/org/jboss/aerogear/connectivity/message/sender/SimplePushNotificationSender.java
+++ src/main/java/org/jboss/aerogear/connectivity/message/sender/SimplePushNotificationSender.java
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * 	http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,66 +17,79 @@
 package org.jboss.aerogear.connectivity.message.sender;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 import java.util.logging.Logger;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import org.jboss.aerogear.connectivity.message.sender.annotations.SimplePushSender;
 
-import com.ning.http.client.AsyncHttpClient;
 import java.io.Serializable;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @SimplePushSender
 @ApplicationScoped
 public class SimplePushNotificationSender implements Serializable {
     private static final long serialVersionUID = 5747687132270998712L;
 
-    private AsyncHttpClient asyncHttpClient;
     @Inject
     private Logger logger;
-
-    @PostConstruct
-    public void createAsyncHttpClient() {
-        asyncHttpClient = new AsyncHttpClient();
-    }
 
     public void sendMessage(String endpoint, String payload, List<String> channels) {
         // iterate over all the given channels:
         for (String channelID : channels) {
 
+            HttpURLConnection conn = null;
             try {
-                // blocking IO here:
-                com.ning.http.client.Response response =
-                        asyncHttpClient.preparePut(endpoint + channelID)
-                                .addHeader("Accept", "application/x-www-form-urlencoded")
-                                .setBody(payload) // should be a string like 'version=123'
-                                .execute().get();
-
-                int simplePushStatusCode = response.getStatusCode();
+                // PUT the version payload to the SimplePushServer 
+                conn = put(endpoint+channelID, payload);
+                int simplePushStatusCode = conn.getResponseCode();
                 logger.info("SimplePush Status: " + simplePushStatusCode);
+
                 if (200 != simplePushStatusCode) {
                     logger.severe("ERROR ??????     STATUS CODE, from PUSH NETWORK was NOT 200, but....: " + simplePushStatusCode);
                 }
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            } catch (IllegalArgumentException e) {
-                e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
+            } finally {
+                // tear down
+                if (conn != null ) {
+                    conn.disconnect();
+                }
             }
         }
     }
 
-    @PreDestroy
-    public void closeAsyncHttpClient() {
-        asyncHttpClient.close();
+    /**
+     * Returns HttpURLConnection that 'puts' the given body to the given URL.
+     */
+    protected HttpURLConnection put(String url, String body) throws IOException {
+
+        if (url == null || body == null) {
+            throw new IllegalArgumentException("arguments cannot be null");
+        }
+
+        byte[] bytes = body.getBytes();
+        HttpURLConnection conn = getConnection(url);
+        conn.setDoOutput(true);
+        conn.setUseCaches(false);
+        conn.setFixedLengthStreamingMode(bytes.length);
+        conn.setRequestProperty("Accept", "application/x-www-form-urlencoded");
+        conn.setRequestMethod("PUT");
+        OutputStream out = conn.getOutputStream();
+        out.write(bytes);
+        out.close();
+        return conn;
+    }
+
+    /**
+     * Convenience method to open/establish a HttpURLConnection.
+     */
+    protected HttpURLConnection getConnection(String url) throws IOException {
+        HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+        return conn;
     }
 }
