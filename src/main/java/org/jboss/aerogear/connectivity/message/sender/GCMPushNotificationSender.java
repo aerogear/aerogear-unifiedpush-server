a44b5adc1b4e9e35284b23c9521cb4bca98b964e
Matthias Wessendorf <matzew@apache.org>
2013-07-24 11:29:55 +0200

    Removing unused import

--- src/main/java/org/jboss/aerogear/connectivity/message/sender/GCMPushNotificationSender.java
+++ src/main/java/org/jboss/aerogear/connectivity/message/sender/GCMPushNotificationSender.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
