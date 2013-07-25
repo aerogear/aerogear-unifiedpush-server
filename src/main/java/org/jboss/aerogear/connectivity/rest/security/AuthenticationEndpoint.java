d4bd47cb5c80ac5579de74f36ba56acc554cba97
Lucas Holmquist <lholmqui@redhat.com>
2013-07-25 13:00:54 -0400

    remove * from imports

--- src/main/java/org/jboss/aerogear/connectivity/rest/security/AuthenticationEndpoint.java
+++ src/main/java/org/jboss/aerogear/connectivity/rest/security/AuthenticationEndpoint.java
@@ -30,7 +30,11 @@ import org.picketlink.idm.model.SimpleUser;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
