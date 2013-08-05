c4c00ca19df59f4035d5088b3a7ea2694f7038f3
Lucas Holmquist <lholmqui@redhat.com>
2013-08-05 14:21:19 -0400

    remove unneeded function

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -30,20 +30,3 @@ App.baseURL = ( function() {
     return window.location.protocol + "//" + window.location.host + pathName + "/";
 })();
 
-
-// Serializes a form to a JavaScript Object
-$.fn.serializeObject = function() {
-    var o = {};
-    var a = this.serializeArray();
-    $.each( a, function() {
-        if ( o[ this.name ] ) {
-            if ( !o[ this.name ].push ) {
-                o[ this.name ] = [ o[ this.name ] ];
-            }
-            o[ this.name ].push( this.value || '' );
-        } else {
-            o[ this.name ] = this.value || '';
-        }
-    });
-    return o;
-};
