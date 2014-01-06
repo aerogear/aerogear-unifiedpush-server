47d604d6a1a51b81babb8d32364235076fc1ba80
Sebastien Blanc <scm.blanc@gmail.com>
2014-01-06 09:22:30 +0100

    Typo fix and DRYing out fixed title part

--- app/scripts/helpers/handlebars_helper.js
+++ app/scripts/helpers/handlebars_helper.js
@@ -1,29 +1,29 @@
 Ember.Handlebars.helper( 'headAppsTitle', function(numberOfApps) {
     if(numberOfApps > 0){
-        Ember.$('head').find('title').text("Applications ("+ numberOfApps +") - AeroGear Unified Push Server");
+        Ember.$('head').find('title').text("Applications ("+ numberOfApps +") - AeroGear UnifiedPush Server");
     }
 }, 'title');
 
 //these could be probably DRYed out
-
+var static_title = " - AeroGear UnifiedPush Server";
 Ember.Handlebars.helper('headAppTitle', function( appName ) {
-    Ember.$('head').find('title').text("Applications "+ appName +" - AeroGear Unified Push Server");
+    Ember.$('head').find('title').text("Applications "+ appName + static_title);
 }, 'title');
 
 Ember.Handlebars.helper('headEditAppTitle', function( appName, action ) {
-    Ember.$('head').find('title').text(action + " " + appName +" - AeroGear Unified Push Server");
+    Ember.$('head').find('title').text(action + " " + appName + static_title);
 }, 'title');
 
 Ember.Handlebars.helper('headVariantTitle', function( variantName ) {
-    Ember.$('head').find('title').text("Variant " + variantName+ " - AeroGear Unified Push Server");
+    Ember.$('head').find('title').text("Variant " + variantName + static_title);
 }, 'title');
 
 Ember.Handlebars.helper('headEditVariantTitle', function( variantName, action ) {
-    Ember.$('head').find('title').text(action + " " + variantName+ " - AeroGear Unified Push Server");
+    Ember.$('head').find('title').text(action + " " + variantName + static_title);
 }, 'title');
 
 
 Ember.Handlebars.helper('headInstallationTitle', function(deviceToken) {
-    Ember.$('head').find('title').text("Device " + deviceToken+ " - AeroGear Unified Push Server");
+    Ember.$('head').find('title').text("Device " + deviceToken + static_title);
 }, 'title');
 
