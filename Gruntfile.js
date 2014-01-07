3b0322c988e1a6e372e75d65121aee10401f01b8
Sebastien Blanc <scm.blanc@gmail.com>
2014-01-07 11:51:25 +0100

    add call do config init before running server

--- Gruntfile.js
+++ Gruntfile.js
@@ -342,6 +342,7 @@ module.exports = function (grunt) {
         }
 
         grunt.task.run([
+            'initLocalConfig',
             'clean:server',
             'concurrent:server',
             'emberTemplates',
