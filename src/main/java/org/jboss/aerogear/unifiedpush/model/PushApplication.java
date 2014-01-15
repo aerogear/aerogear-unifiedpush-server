51bcdd568875785a31d56baff6c7639c865ba08b
Sebastien Blanc <scm.blanc@gmail.com>
2014-01-15 10:25:36 +0100

    removed unused import

--- src/main/java/org/jboss/aerogear/unifiedpush/model/PushApplication.java
+++ src/main/java/org/jboss/aerogear/unifiedpush/model/PushApplication.java
@@ -28,8 +28,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
-import org.codehaus.jackson.annotate.JsonProperty;
 import org.jboss.aerogear.unifiedpush.jpa.PersistentObject;
 
 @Entity
