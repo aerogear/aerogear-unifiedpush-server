1bebd01591007eb8bb8b85a4e8366a9ccffdb3dc
Sebastien Blanc <scm.blanc@gmail.com>
2014-04-16 08:20:09 +0200

    admin using angular

---
+++ newadmin/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('newadminApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
