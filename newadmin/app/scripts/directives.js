bdb678fed01d989828e64e3910484b2690b23712
Erik Jan de Wit <erikjan.dewit@gmail.com>
2014-04-29 18:12:24 +0200

    initial version of the details page with hbons styles

--- newadmin/app/scripts/directives.js
+++ newadmin/app/scripts/directives.js
@@ -12,4 +12,25 @@ angular.module('ups.directives', [])
             replace: true,
             templateUrl: 'directives/ups-navigation.html'
         };
+    })
+
+    .directive('variants', function () {
+        return {
+            scope: {
+                variants: '='
+            },
+            controller: function($scope, $window) {
+                $scope.expand = function(variant) {
+                    variant.expand = !variant.expand;
+                };
+
+                $scope.isCollapsed = function(variant) {
+                    return !variant.expand;
+                };
+
+                var href = $window.location.href;
+                $scope.currentLocation = href.substring(0, href.indexOf('#'));
+            },
+            templateUrl: 'directives/variant-details.html'
+        };
     });
