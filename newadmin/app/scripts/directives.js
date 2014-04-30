a11ba7dba835e8986d73ca80c744dd758bac22e3
Erik Jan de Wit <erikjan.dewit@gmail.com>
2014-04-30 07:48:34 +0200

    use the same controller

--- newadmin/app/scripts/directives.js
+++ newadmin/app/scripts/directives.js
@@ -19,18 +19,7 @@ angular.module('ups.directives', [])
             scope: {
                 variants: '='
             },
-            controller: function($scope, $window) {
-                $scope.expand = function(variant) {
-                    variant.expand = !variant.expand;
-                };
-
-                $scope.isCollapsed = function(variant) {
-                    return !variant.expand;
-                };
-
-                var href = $window.location.href;
-                $scope.currentLocation = href.substring(0, href.indexOf('#'));
-            },
+            controller: 'DetailController',
             templateUrl: 'directives/variant-details.html'
         };
     });
