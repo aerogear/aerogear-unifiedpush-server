debeac917fac3ce6e26e5163c75bcae41684bba8
Lukas Fryc <lfryc@redhat.com>
2014-04-30 12:42:11 +0200

    watch HTML changes in deep directory structures

--- newadmin/Gruntfile.js
+++ newadmin/Gruntfile.js
@@ -44,7 +44,7 @@ module.exports = function (grunt) {
           livereload: LIVERELOAD_PORT
         },
         files: [
-          '<%= yeoman.app %>/{,*/}*.html',
+          '<%= yeoman.app %>/**/*.html',
           '.tmp/styles/{,*/}*.css',
           '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
           '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
