d808af711600679309b5edb6ac9778cc4a50365e
Lukas Fryc <lfryc@redhat.com>
2014-04-30 15:28:20 +0200

    AGPUSH-628: base for Add Variant dialog

--- newadmin/app/scripts/controllers/DetailController.js
+++ newadmin/app/scripts/controllers/DetailController.js
@@ -16,7 +16,23 @@
  */
 'use strict';
 
-function DetailController($scope, $routeParams, $window, $modal, pushApplication) {
+function DetailController($scope, $routeParams, $window, $modal, pushApplication, variants) {
+
+    /*
+     * INITIALIZATION
+     */
+
+    pushApplication.get({appId: $routeParams.applicationId}, function(application) {
+        $scope.application = application;
+        var href = $window.location.href;
+        $scope.currentLocation = href.substring(0, href.indexOf('#'));
+    });
+
+
+    /*
+     * PUBLIC METHODS
+     */
+
     $scope.expand = function(variant) {
         variant.expand = !variant.expand;
     };
@@ -25,9 +41,70 @@ function DetailController($scope, $routeParams, $window, $modal, pushApplication
         return !variant.expand;
     };
 
-    pushApplication.get({appId: $routeParams.applicationId}, function(application) {
-        $scope.application = application;
-        var href = $window.location.href;
-        $scope.currentLocation = href.substring(0, href.indexOf('#'));
-    });
+    $scope.addVariant = function (variant) {
+        var modalInstance = show(variant, 'create-variant.html');
+        modalInstance.result.then(function (variant) {
+            pushApplication.create(variant, function(newVariant) {
+                $scope.applications.push(newVariant);
+                createAlert("Successfully created variant \"" + newVariant.name + "\"");
+            }, function() {
+                createAlert("Something went wrong...", "danger");
+            });
+        });
+    };
+
+//    $scope.editVariant = function(application) {
+//        var modalInstance = show(application, 'create-app.html');
+//        modalInstance.result.then(function (application) {
+//            pushApplication.update({appId:application.pushApplicationID}, application, function() {
+//                createAlert("Successfully edited application \"" + application.name + "\"");
+//            });
+//        });
+//    };
+//
+//    $scope.removeVariant = function(application) {
+//        var modalInstance = show(application, 'remove-app.html');
+//        modalInstance.result.then(function () {
+//            pushApplication.remove({appId:application.pushApplicationID}, function() {
+//                createAlert("Successfully removed application \"" + application.name + "\"");
+//                $scope.applications.splice($scope.applications.indexOf(application), 1);
+//            });
+//        });
+//    };
+
+    /*
+     * PRIVATE FUNCTIONS
+     */
+
+    function modalController($scope, $modalInstance, variant) {
+        $scope.variant = variant;
+        $scope.ok = function (variant) {
+            $modalInstance.close(variant);
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    }
+
+    function show(variant, template) {
+        return $modal.open({
+            templateUrl: 'views/dialogs/' + template,
+            controller: modalController,
+            resolve: {
+                variant: function () {
+                    return variant;
+                }
+            }
+        });
+    }
+
+    function createAlert(msg, type) {
+        $scope.alerts.push({type: type || 'success', msg: msg});
+        setTimeout(function () {
+            $scope.alerts.splice(0, 1);
+            $scope.$apply();
+        }, 4000)
+    }
+
 }
