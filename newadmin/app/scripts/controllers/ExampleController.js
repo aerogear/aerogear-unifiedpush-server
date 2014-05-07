4a7a1548630f81cfa5232e6fb762c77600c8ae04
Erik Jan de Wit <erikjan.dewit@gmail.com>
2014-05-07 17:00:05 +0200

    initial version of example page

---
+++ newadmin/app/scripts/controllers/ExampleController.js
@@ -0,0 +1,38 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright Red Hat, Inc., and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+function ExampleController($rootScope, $scope, $routeParams, $window, variants) {
+
+    /*
+     * INITIALIZATION
+     */
+    onLoginDone($rootScope, $scope, function() {
+        var params = {
+            appId: $routeParams.applicationId,
+            variantType: $routeParams.variantType,
+            variantId: $routeParams.variantId
+        };
+        variants.get(params, function(variant) {
+            $scope.variant = variant;
+            $scope.variantType = $routeParams.variantType;
+            $scope.applicationId = $routeParams.applicationId;
+            var href = $window.location.href;
+            $scope.currentLocation = href.substring(0, href.indexOf('#'));
+        });
+    });
+}
