84f5f27b678d3310b8699f6a8c1537f6c8bb52c2
Erik Jan de Wit <erikjan.dewit@gmail.com>
2014-05-12 15:08:38 +0200

    refactored into separate class to prevent class loading issues

---
+++ jaxrs/src/main/java/org/jboss/aerogear/unifiedpush/rest/util/ObjectMapperContextResolver.java
@@ -0,0 +1,20 @@
+package org.jboss.aerogear.unifiedpush.rest.util;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.xc.JaxbAnnotationIntrospector;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ObjectMapperContextResolver implements ContextResolver<ObjectMapper> {
+    private ObjectMapper objectMapper;
+
+    public ObjectMapper getContext(Class<?> objectType) {
+        if (objectMapper == null) {
+            objectMapper = new ObjectMapper();
+            objectMapper.setAnnotationIntrospector(new JaxbAnnotationIntrospector());
+        }
+        return objectMapper;
+    }
+}
