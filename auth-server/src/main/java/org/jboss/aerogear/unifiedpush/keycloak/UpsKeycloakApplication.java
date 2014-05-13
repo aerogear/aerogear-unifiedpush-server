9254391f88d0514728ae74cc9ccd3b94de471ecd
Matthias Wessendorf <matzew@apache.org>
2014-05-13 17:38:00 +0200

    Integration w/ Keycloak via custom auth-server WAR

---
+++ auth-server/src/main/java/org/jboss/aerogear/unifiedpush/keycloak/UpsKeycloakApplication.java
@@ -0,0 +1,56 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright Red Hat, Inc., and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.aerogear.unifiedpush.keycloak;
+
+import org.jboss.resteasy.core.Dispatcher;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.services.managers.RealmManager;
+import org.keycloak.services.resources.KeycloakApplication;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.Context;
+
+/**
+ * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
+ * @version $Revision: 1 $
+ */
+public class UpsKeycloakApplication extends KeycloakApplication {
+    public UpsKeycloakApplication(@Context ServletContext context, @Context Dispatcher dispatcher) {
+        super(context, dispatcher);
+    }
+
+    @Override
+    protected void setupDefaultRealm(String contextPath) {
+        super.setupDefaultRealm(contextPath);
+        KeycloakSession session = factory.createSession();
+        session.getTransaction().begin();
+
+        // disable master realm by deleting the admin user.
+        try {
+            RealmManager manager = new RealmManager(session);
+            RealmModel master = manager.getKeycloakAdminstrationRealm();
+            UserModel admin = master.getUser("admin");
+            if (admin != null) master.removeUser(admin.getLoginName());
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
+
+    }
+}
