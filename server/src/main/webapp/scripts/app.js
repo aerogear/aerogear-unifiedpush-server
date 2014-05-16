01104542e6e86475e05920b0f4529be447713535
Sebastien Blanc <scm.blanc@gmail.com>
2014-05-16 13:52:10 +0200

    bundle a (not yet optimized) console dist in webapp

---
+++ server/src/main/webapp/scripts/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('newadminApp', [
+  'newadminApp.services',
+  'ngResource',
+  'ngRoute',
+  'ui.bootstrap',
+  'ups.directives',
+  'patternfly.notification'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainController'
+      })
+      .when('/detail/:applicationId', {
+        templateUrl: 'views/detail.html',
+        controller: 'DetailController'
+      })
+      .when('/installations/:variantId', {
+        templateUrl: 'views/installation.html',
+        controller: 'InstallationController'
+      })
+      .when('/example/:applicationId/:variantType/:variantId', {
+        templateUrl: 'views/example.html',
+        controller: 'ExampleController'
+      })
+      .when('/example/:applicationId/:variantType', {
+        templateUrl: 'views/example.html',
+        controller: 'ExampleController'
+      })
+      .when('/compose/:applicationId', {
+        templateUrl: 'views/compose.html',
+        controller: 'ComposeController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+  });
+
