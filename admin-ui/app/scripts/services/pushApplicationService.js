a51cedafe6cd1ddf58059adb20a80889d79307a5
Erik Jan de Wit <erikjan.dewit@gmail.com>
2014-05-27 15:09:39 +0200

    fixed breadcrumbs

--- admin-ui/app/scripts/services/pushApplicationService.js
+++ admin-ui/app/scripts/services/pushApplicationService.js
@@ -82,9 +82,66 @@ backendMod.factory('installations', function ($resource) {
   });
 });
 
+backendMod.factory('breadcrumbs', function ($rootScope, $route) {
+  var BreadcrumbService = {
+    breadcrumbs: [],
+    get: function() {
+      return this.breadcrumbs;
+    },
+    generateBreadcrumbs: function() {
+      var routes = $route.routes,
+        self = this;
 
+      var getRoute = function(route) {
+        if ($route.current) {
+          var param;
+          angular.forEach($route.current.params, function (value, key) {
+            if (route.indexOf(key) !== -1) {
+              param = value;
+            }
+            if (param) {
+              route = route.replace(':' + key, value);
+            }
+          });
+          return route;
+        }
+      };
 
+      var getRouteByLevel = function(level) {
+        var result = null;
+        angular.forEach(routes, function(route) {
+          if (route.crumb && route.crumb.level === level) {
+            result = route;
+          }
+        });
+        return result;
+      };
 
+      var label = function(route) {
+        var label = route.crumb.label.indexOf('$') !== -1 ? $rootScope.$eval(route.crumb.label.substring(1)) : route.crumb.label;
+        self.breadcrumbs.push({ label: label, path: route.path });
+      }
+      
+      this.breadcrumbs = [];
+      if ($route.current && $route.current.crumb) {
+        label($route.current);
+        for (var i = $route.current.crumb.level - 1; i >= 0 ; i--) {
+          var route = getRouteByLevel(i);
+          route.path = getRoute(route.originalPath);
+          label(route);
+        }
+        self.breadcrumbs.reverse();
+      }
+    }
+  };
 
+  // We want to update breadcrumbs only when a route is actually changed
+  // as $location.path() will get updated immediately (even if route change fails!)
+  $rootScope.$on('$routeChangeSuccess', function() {
+    BreadcrumbService.generateBreadcrumbs();
+  });
 
+  BreadcrumbService.generateBreadcrumbs();
 
+  return BreadcrumbService;
+});
