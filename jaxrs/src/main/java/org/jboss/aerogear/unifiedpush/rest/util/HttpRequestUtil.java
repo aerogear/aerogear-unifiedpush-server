5f1fc95833bd203985c34a788fb19548faf0c5cf
Daniel Bevenius <daniel.bevenius@gmail.com>
2014-05-28 11:54:58 +0200

    Refactoring extractIPAddress to avoid addition if checks.

--- jaxrs/src/main/java/org/jboss/aerogear/unifiedpush/rest/util/HttpRequestUtil.java
+++ jaxrs/src/main/java/org/jboss/aerogear/unifiedpush/rest/util/HttpRequestUtil.java
@@ -32,16 +32,21 @@ public final class HttpRequestUtil {
      */
     public static String extractIPAddress(final HttpServletRequest request) {
         String ip = request.getHeader("x-forwarded-for");
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("Proxy-Client-IP");
+        if (hasValue(ip)) {
+            return ip;
         }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
+        ip = request.getHeader("Proxy-Client-IP");
+        if (hasValue(ip)) {
+            return ip;
         }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
+        ip = request.getHeader("WL-Proxy-Client-IP");
+        if (hasValue(ip)) {
+            return ip;
         }
+        return request.getRemoteAddr();
+    }
 
-        return ip;
+    private static boolean hasValue(String value) {
+        return value != null && !value.isEmpty() && !"unknown".equalsIgnoreCase(value);
     }
 }
