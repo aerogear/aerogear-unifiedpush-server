1f51e738d57349ecf12cf9062ade7dbd7f7e944f
Lukas Fryc <lfryc@redhat.com>
2014-06-03 14:59:09 +0200

    UX: focus first input in a modal once modal is shown

--- admin-ui/app/scripts/ui/bootstrap-customization.js
+++ admin-ui/app/scripts/ui/bootstrap-customization.js
@@ -7,4 +7,29 @@
     $('.dropdown.open').removeClass('open');
   });
 
+  // focus first input in a modal once modal is shown
+  angular.module('newadminApp')
+    .config(function($provide) {
+      $provide.decorator('$modal', function($delegate) {
+        return {
+          open: function() {
+            var modal = $delegate.open.apply(this, arguments);
+            modal.opened.then(function() {
+              (function delayFocus() {
+                var el = $('.modal-dialog input:first:visible');
+                if (el.size() > 0) {
+                  window.requestAnimationFrame(function() {
+                    el.focus();
+                  });
+                } else {
+                  window.setTimeout(delayFocus, 50);
+                }
+              })();
+            });
+            return modal;
+          }
+        };
+      });
+    });
+
 })($);
