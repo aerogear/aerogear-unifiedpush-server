1d678f90cad47dcb2795a4eb7322b701d0a2a49d
Lukas Fryc <lfryc@redhat.com>
2014-06-03 11:57:30 +0200

    fixed link to sidebar-active.svg image

--- admin-ui/Gruntfile.js
+++ admin-ui/Gruntfile.js
@@ -105,7 +105,7 @@ module.exports = function (grunt) {
           src: [
             '<%= yeoman.dist %>/scripts/{,*/}*.js',
             '<%= yeoman.dist %>/styles/{,*/}*.css',
-            '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+            '<%= yeoman.dist %>/img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
             '<%= yeoman.dist %>/styles/fonts/*'
           ]
         }
