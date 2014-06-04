aeb0c3943a23ce69e43c852d9c7144e83161fcc4
Lukas Fryc <lfryc@redhat.com>
2014-06-04 13:33:20 +0200

    reformat directives.js code with indent=2

--- admin-ui/app/scripts/directives.js
+++ admin-ui/app/scripts/directives.js
@@ -3,16 +3,16 @@
 /* Directives */
 angular.module('ups.directives', [])
 
-    .directive('upsNavigation', function () {
-        return {
-            scope: {
-                current: '@'
-              },
-              restrict: 'E',
-              replace: true,
-              templateUrl: 'directives/ups-navigation.html'
-            };
-      })
+  .directive('upsNavigation', function () {
+    return {
+      scope: {
+        current: '@'
+      },
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'directives/ups-navigation.html'
+    };
+  })
 
   .directive('upsAlerts', function () {
     return {
@@ -29,65 +29,65 @@ angular.module('ups.directives', [])
   .directive('upsBreadcrumb', function () {
     return {
       templateUrl: 'directives/ups-breadcrumb.html',
-      controller: function($scope, $compile, breadcrumbs) {
+      controller: function ($scope, $compile, breadcrumbs) {
         $scope.breadcrumbs = breadcrumbs;
       }
     };
   })
 
-    .directive('variants', function () {
-        return {
-            scope: {
-                variants: '=',
-                counts: '=',
-                type: '@'
-              },
-              controller: function($rootScope, $scope, $routeParams) {
-                $scope.expand = function(variant) {
-                    variant.expand = !variant.expand;
-                  };
+  .directive('variants', function () {
+    return {
+      scope: {
+        variants: '=',
+        counts: '=',
+        type: '@'
+      },
+      controller: function ($rootScope, $scope, $routeParams) {
+        $scope.expand = function (variant) {
+          variant.expand = !variant.expand;
+        };
 
-                $scope.isCollapsed = function(variant) {
-                    return !variant.expand;
-                  };
+        $scope.isCollapsed = function (variant) {
+          return !variant.expand;
+        };
 
-                $scope.editVariant = function(variant, type) {
-                    $scope.$parent.editVariant(variant, type);
-                  };
+        $scope.editVariant = function (variant, type) {
+          $scope.$parent.editVariant(variant, type);
+        };
 
-                $scope.removeVariant = function(variant, type) {
-                    $scope.$parent.removeVariant(variant, type);
-                  };
+        $scope.removeVariant = function (variant, type) {
+          $scope.$parent.removeVariant(variant, type);
+        };
 
-                $scope.applicationId = $routeParams.applicationId;
-                
-                $scope.currentVariant = function (variant) {
-                  $rootScope.variant = variant;
-                };
-                
-              },
-              templateUrl: 'directives/variant-details.html'
-            };
-      })
+        $scope.applicationId = $routeParams.applicationId;
 
-    .directive('upsFiles', function() {
-        return {
-            scope: {
-                'files': '=upsFiles'
-              },
-              restrict: 'A',
-              replace: false,
-              link: function($scope, $element) {
-                  $element.bind('change', function(e) {
-                    while($scope.files.length > 0) {
-                      $scope.files.pop();
-                    }
-                    for(var i in e.target.files) {
-                      if(typeof e.target.files[i] === 'object') {
-                        $scope.files.push(e.target.files[i]);
-                      }
-                    }
-                  });
-                }
-            };
-      });
+        $scope.currentVariant = function (variant) {
+          $rootScope.variant = variant;
+        };
+
+      },
+      templateUrl: 'directives/variant-details.html'
+    };
+  })
+
+  .directive('upsFiles', function () {
+    return {
+      scope: {
+        'files': '=upsFiles'
+      },
+      restrict: 'A',
+      replace: false,
+      link: function ($scope, $element) {
+        $element.bind('change', function (e) {
+          while ($scope.files.length > 0) {
+            $scope.files.pop();
+          }
+          for (var i in e.target.files) {
+            if (typeof e.target.files[i] === 'object') {
+              $scope.files.push(e.target.files[i]);
+            }
+          }
+        });
+      }
+    };
+  });
