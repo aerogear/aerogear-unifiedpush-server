b8683d129cf2dcc70823accf141e5e0395d0d48b
Erik Jan de Wit <erikjan.dewit@gmail.com>
2014-06-05 09:42:02 +0200

    add pages

--- admin-ui/app/scripts/app.js
+++ admin-ui/app/scripts/app.js
@@ -88,6 +88,23 @@ angular.module('newadminApp', [
 =======
 >>>>>>> support for multiple roots on breadcrumbs
       })
+      .when('/activity/:applicationId', {
+        templateUrl: 'views/notification.html',
+        controller: 'DashboardController',
+        crumb: {
+          id: 'activity',
+          parent: 'dash',
+          label: '$ application.name ? application.name : "Current Application"'
+        }
+      })
+      .when('/activity/:applicationId/:variantId', {
+        templateUrl: 'views/notification.html',
+        controller: 'DashboardController',
+        crumb: {
+          parent: 'activity',
+          label: '$ variant.name ? variant.name : "Current variant"'
+        }
+      })
       .otherwise({
         redirectTo: '/'
       });
