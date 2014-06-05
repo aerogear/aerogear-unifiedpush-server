7fd32409bcb857f84f4100b960a4dac62d18b966
Erik Jan de Wit <erikjan.dewit@gmail.com>
2014-06-05 09:12:29 +0200

    support for multiple roots on breadcrumbs

--- admin-ui/app/scripts/services/pushApplicationService.js
+++ admin-ui/app/scripts/services/pushApplicationService.js
@@ -93,12 +93,20 @@ backendMod.factory('dashboard', function ($resource) {
 backendMod.factory('breadcrumbs', function ($rootScope, $route) {
   var BreadcrumbService = {
     breadcrumbs: [],
+    routes: {},
     get: function() {
       return this.breadcrumbs;
     },
+    init: function() {
+      var self = this;
+      angular.forEach($route.routes, function(route) {
+        if (route.crumb) {
+          self.routes[route.crumb.id] = route;
+        }
+      });
+    },
     generateBreadcrumbs: function() {
-      var routes = $route.routes,
-        self = this;
+      var parent, self = this;
 
       var getRoute = function(route) {
         if ($route.current) {
@@ -115,29 +123,22 @@ backendMod.factory('breadcrumbs', function ($rootScope, $route) {
         }
       };
 
-      var getRouteByLevel = function(level) {
-        var result = null;
-        angular.forEach(routes, function(route) {
-          if (route.crumb && route.crumb.level === level) {
-            result = route;
-          }
-        });
-        return result;
+      var label = function(route) {
+        return route.crumb.label.indexOf('$') !== -1 ? $rootScope.$eval(route.crumb.label.substring(1)) : route.crumb.label;
       };
 
-      var label = function(route) {
-        var label = route.crumb.label.indexOf('$') !== -1 ? $rootScope.$eval(route.crumb.label.substring(1)) : route.crumb.label;
-        self.breadcrumbs.push({ label: label, path: route.path });
-      };
-      
       this.breadcrumbs = [];
       if ($route.current && $route.current.crumb) {
-        label($route.current);
-        for (var i = $route.current.crumb.level - 1; i >= 0 ; i--) {
-          var route = getRouteByLevel(i);
+        self.breadcrumbs.push({ label: label($route.current), path: $route.current.path });
+        parent = $route.current.crumb.parent;
+
+        while (parent) {
+          var route = self.routes[parent];
           route.path = getRoute(route.originalPath);
-          label(route);
+          self.breadcrumbs.push({ label: label(route), path: route.path });
+          parent = route.crumb.parent;
         }
+
         self.breadcrumbs.reverse();
       }
     }
@@ -149,6 +150,7 @@ backendMod.factory('breadcrumbs', function ($rootScope, $route) {
     BreadcrumbService.generateBreadcrumbs();
   });
 
+  BreadcrumbService.init();
   BreadcrumbService.generateBreadcrumbs();
 
   return BreadcrumbService;
