30e7558ceeda37680645fb953ee10c650cdc029d
Sebastien Blanc <scm.blanc@gmail.com>
2014-06-08 11:30:20 +0200

    Change keycloak.js flow and add interceptors

--- admin-ui/app/scripts/app.js
+++ admin-ui/app/scripts/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var module = angular.module('newadminApp', [
 angular.module('upsConsole', [
   'upsConsole.services',
   'ngResource',
@@ -8,8 +8,39 @@ angular.module('upsConsole', [
   'ups.directives',
   'patternfly.notification',
   'hljs'
-])
-  .config(function ($routeProvider) {
+var auth = {};
+var logout = function(){
+  console.log('*** LOGOUT');
+  auth.loggedIn = false;
+  auth.authz = null;
+  window.location = auth.logoutUrl;
+};
+
+
+angular.element(document).ready(function ($http) {
+  var keycloakAuth = new Keycloak('keycloak.json');
+  auth.loggedIn = false;
+
+  keycloakAuth.init({ onLoad: 'login-required' }).success(function () {
+    auth.loggedIn = true;
+    auth.authz = keycloakAuth;
+    auth.logoutUrl = keycloakAuth.authServerUrl + "/realms/demo/tokens/logout?redirect_uri=http://localhost:8080/angular-product/index.html";
+    module.factory('Auth', function() {
+      return auth;
+    });
+    window.location = "#/main"
+  }).error(function () {
+    window.location.reload();
+  });
+
+});
+
+module.factory('Auth', function() {
+  return auth;
+});
+
+module.config(function ($routeProvider) {
+
     $routeProvider
       .when('/applications', {
         templateUrl: 'views/applications.html',
@@ -131,9 +162,31 @@ angular.module('upsConsole', [
           label: '$ variant.name ? variant.name : "Current variant"'
         }
       })
-      .otherwise({
+
         redirectTo: '/dashboard'
-      });
+  }) ;
+module.factory('authInterceptor', function($q, Auth) {
+  return {
+    request: function (config) {
+      var deferred = $q.defer();
+      if (Auth.authz.token) {
+        Auth.authz.updateToken(5).success(function() {
+          config.headers = config.headers || {};
+          config.headers.Authorization = 'Bearer ' + Auth.authz.token;
+
+          deferred.resolve(config);
+        }).error(function() {
+          deferred.reject('Failed to refresh token');
+        });
+      }
+      return deferred.promise;
+    }
+  };
+});
+
+ module.config(function($httpProvider) {
+    //$httpProvider.responseInterceptors.push('errorInterceptor');
+    $httpProvider.interceptors.push('authInterceptor');
 
   });
 
