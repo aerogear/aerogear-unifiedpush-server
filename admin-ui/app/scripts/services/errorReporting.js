d7c0acaa416265f3a781683fe32bc456e4a0d08f
Lukas Fryc <lfryc@redhat.com>
2014-06-16 14:10:47 +0200

    added general server response error handling mechanism AGPUSH-720

---
+++ admin-ui/app/scripts/services/errorReporting.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('upsConsole')
+
+  .config(function($httpProvider) {
+    $httpProvider.interceptors.push(function($q, $interpolate, Notifications) {
+      return {
+        'responseError': function(rejection) {
+          var expression = $interpolate('Server returned {{status}}: {{statusText}}');
+          Notifications.error(expression(rejection));
+          return $q.reject(rejection);
+        }
+      };
+    });
+  });
