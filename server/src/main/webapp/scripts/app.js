78398e530f681e0181a422b27c08aa47c13b500f
Bruno Oliveira <bruno@abstractj.org>
2014-06-24 15:32:01 -0400

    Inclusion of admin-ui on dist

---
+++ server/src/main/webapp/scripts/app.js
@@ -0,0 +1,157 @@
+'use strict';
+var module = angular.module('upsConsole', [
+  'upsConsole.services',
+  'ngResource',
+  'ngRoute',
+  'ui.bootstrap',
+  'ups.directives',
+  'patternfly.notification',
+  'hljs'
+])
+var auth = {};
+var logout = function(){
+  console.log('*** LOGOUT');
+  auth.loggedIn = false;
+  auth.authz = null;
+  window.location = auth.logoutUrl;
+};
+
+
+angular.element(document).ready(function ($http) {
+  var keycloakAuth = new Keycloak('keycloak.json');
+  auth.loggedIn = false;
+
+  keycloakAuth.init({ onLoad: 'login-required' }).success(function () {
+    auth.loggedIn = true;
+    auth.authz = keycloakAuth;
+    auth.logoutUrl = keycloakAuth.authServerUrl + "/realms/demo/tokens/logout?redirect_uri=http://localhost:8080/angular-product/index.html";
+    module.factory('Auth', function() {
+      return auth;
+    });
+    window.location = "#/main";
+    angular.bootstrap(document, ['upsConsole']);
+  }).error(function () {
+    window.location.reload();
+  });
+
+});
+
+module.factory('Auth', function() {
+  return auth;
+});
+
+module.config(function ($routeProvider) {
+
+    $routeProvider
+      .when('/main', {
+        templateUrl: 'views/main.html',
+        controller: 'MainController',
+        crumb: {
+          id: 'apps',
+          label: 'Applications'
+        }
+      })
+      .when('/detail/:applicationId', {
+        templateUrl: 'views/detail.html',
+        controller: 'DetailController',
+        crumb: {
+          id: 'app-detail',
+          parent: 'apps',
+          label: '$ application.name ? application.name : "Current Application"'
+        }
+      })
+      .when('/:applicationId/installations/:variantId', {
+        templateUrl: 'views/installation.html',
+        controller: 'InstallationController',
+        crumb: {
+          parent: 'app-detail',
+          label: '$ variant.name ? variant.name : "Registering Installations"'
+        }
+      })
+      .when('/example/:applicationId/:variantType/:variantId', {
+        templateUrl: 'views/example.html',
+        controller: 'ExampleController',
+        crumb: {
+          parent: 'app-detail',
+          label: 'Example'
+        }
+      })
+      .when('/example/:applicationId/:variantType', {
+        templateUrl: 'views/example.html',
+        controller: 'ExampleController',
+        crumb: {
+          parent: 'app-detail',
+          label: 'Example'
+        }
+      })
+      .when('/compose', {
+        templateUrl: 'views/compose-app.html',
+        controller: 'PreComposeController',
+        crumb: {
+          label: 'Send Push'
+        }
+      })
+      .when('/compose/:applicationId', {
+        templateUrl: 'views/compose.html',
+        controller: 'ComposeController',
+        crumb: {
+          parent: 'app-detail',
+          label: 'Send Push'
+        }
+      })
+      .when('/dashboard', {
+        templateUrl: 'views/dashboard.html',
+        controller: 'DashboardController',
+        crumb: {
+          id: 'dash',
+          label: 'Dashboard'
+        }
+      })
+      .when('/activity/:applicationId', {
+        templateUrl: 'views/notification.html',
+        controller: 'ActivityController',
+        crumb: {
+          id: 'activity',
+          parent: 'dash',
+          label: '$ application.name ? application.name : "Current Application"'
+        }
+      })
+      .when('/activity/:applicationId/:variantId', {
+        templateUrl: 'views/notification.html',
+        controller: 'ActivityController',
+        crumb: {
+          parent: 'activity',
+          label: '$ variant.name ? variant.name : "Current variant"'
+        }
+      })
+      .otherwise({
+        redirectTo: '/main'
+      });
+
+
+  }) ;
+module.factory('authInterceptor', function($q, Auth) {
+  return {
+    request: function (config) {
+      var deferred = $q.defer();
+      if (Auth.authz.token) {
+        Auth.authz.updateToken(5).success(function() {
+          config.headers = config.headers || {};
+          config.headers.Authorization = 'Bearer ' + Auth.authz.token;
+
+          deferred.resolve(config);
+        }).error(function() {
+          deferred.reject('Failed to refresh token');
+        });
+      }
+      return deferred.promise;
+    }
+  };
+});
+
+ module.config(function($httpProvider) {
+    //$httpProvider.responseInterceptors.push('errorInterceptor');
+    $httpProvider.interceptors.push('authInterceptor');
+
+  });
+
---
+++ server/src/main/webapp/scripts/controllers/DashboardController.js
@@ -0,0 +1,137 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright Red Hat, Inc., and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+angular.module('upsConsole').controller('DashboardController',
+  function ($rootScope, $scope, dashboard) {
+    $rootScope.application = null;
+    dashboard.totals({}, function (data) {
+      $scope.dashboardData = data;
+    });
+
+    dashboard.warnings({}, function (data) {
+      $scope.warnings = data;
+    });
+
+    dashboard.topThree({}, function (data) {
+      $scope.topThree = data;
+    });
+  });
+
+angular.module('upsConsole').controller('ActivityController',
+  function ($scope, $rootScope, $routeParams, $modal, metrics, pushApplication, breadcrumbs) {
+
+    $scope.applicationId = $routeParams.applicationId;
+
+    function findVariant(variants, closure, variantId) {
+      angular.forEach(variants, function (variant) {
+        if (variant.variantID === variantId) {
+          closure(variant);
+        }
+      });
+    }
+
+    function forAllVariants(application, variantId, closure) {
+      findVariant(application.iosvariants, closure, variantId);
+      findVariant(application.androidVariants, closure, variantId);
+      findVariant(application.simplePushVariants, closure, variantId);
+      findVariant(application.chromePackagedAppVariants, closure, variantId);
+    }
+
+    pushApplication.get({appId: $routeParams.applicationId}, function (application) {
+      $rootScope.application = application;
+
+      if (typeof $routeParams.variantId !== 'undefined') {
+        forAllVariants(application, $routeParams.variantId, function (variant) {
+          $rootScope.variant = variant;
+        });
+      }
+      breadcrumbs.generateBreadcrumbs();
+    });
+
+    if (typeof $routeParams.variantId !== 'undefined') {
+      metrics.variant({id: $routeParams.variantId}, function (data) {
+        $scope.pushMetrics = data;
+        angular.forEach(data, function (metric) {
+          metric.totalReceivers = metric.variantInformations[0].receivers;
+          metric.deliveryFailed = !metric.variantInformations[0].deliveryStatus;
+        });
+      });
+    } else {
+      metrics.application({id: $routeParams.applicationId}, function(data) {
+        $scope.pushMetrics = data;
+
+        function totalReceivers(data) {
+          angular.forEach(data, function (metric) {
+            angular.forEach(metric.variantInformations, function (variant) {
+              if (!variant.deliveryStatus) {
+                metric.deliveryFailed = true;
+              }
+              if (!metric.totalReceivers) {
+                metric.totalReceivers = 0;
+              }
+              metric.totalReceivers += variant.receivers;
+            });
+          });
+        }
+
+        totalReceivers(data);
+      });
+    }
+
+    $scope.variantMetricInformation = function(metrics) {
+      angular.forEach(metrics, function(variantInfo) {
+        forAllVariants($rootScope.application, variantInfo.variantID, function (variant) {
+          variantInfo.name = variant.name;
+        });
+      });
+
+      return metrics;
+    };
+
+    $scope.expand = function (metric) {
+      metric.expand = !metric.expand;
+    };
+
+    $scope.isCollapsed = function (metric) {
+      return !metric.expand;
+    };
+
+    $scope.parse = function (metric) {
+      return JSON.parse(metric.rawJsonMessage);
+    };
+
+    $scope.showFullRequest = function (rawJsonMessage) {
+      $modal.open({
+        templateUrl: 'views/dialogs/request.html',
+        controller: function ($scope, $modalInstance, request) {
+          $scope.request = request;
+
+          $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+          };
+        },
+        resolve: {
+          request: function () {
+            //nasty way to get formatted json
+            return JSON.stringify(JSON.parse(rawJsonMessage), null, 4);
+          }
+        }
+      });
+    };
+
+  });
