66c59a5dec3a3fda1501e3c1894aadd407c6c199
Bruno Oliveira <bruno@abstractj.org>
2014-07-12 13:45:09 -0300

    Introducing timeout due to inactivity

---
+++ admin-ui/app/scripts/services/idleService.js
@@ -0,0 +1,24 @@
+'use strict';
+
+$.idleTimeout('#idletimeout', '#idletimeout a', {
+    idleAfter: 5,
+    pollingInterval: 2,
+    //        keepAliveURL: authUrl + '/admin/keepalive', would need to change this path
+    serverResponseEquals: '',
+    failedRequests: 1,
+    onTimeout: function(){
+        $(this).slideUp();
+        console.log('Logged out');
+      },
+    onIdle: function(){
+        $(this).slideDown(); // show the warning bar
+      },
+    onCountdown: function( counter ){
+        $(this).find('span').html( counter ); // update the counter
+      },
+    onResume: function(){
+        $(this).slideUp(); // hide the warning bar
+      }
+  });
+
+
