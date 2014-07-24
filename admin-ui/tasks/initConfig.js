841ebdf633c81c35ab62a5d7b07073fbba931955
Lukas Fryc <lfryc@redhat.com>
2014-07-24 12:09:04 +0200

    verify that specified jbossweb directory is a correct deployed exploded WAR AGPUSH-834

--- admin-ui/tasks/initConfig.js
+++ admin-ui/tasks/initConfig.js
@@ -2,10 +2,8 @@
 var exec = require('child_process').exec;
 var path = require('path');
 
-
-
 module.exports = function ( grunt ) {
-
+    
     /**
      * Task that loads the local config and merged it with the global config object
      * If the local config file does not exist it will be created.
@@ -24,8 +22,26 @@ module.exports = function ( grunt ) {
         }
         var config = grunt.config.getRaw();
         config.local = grunt.file.readJSON('./local-config.json');
+        
+        verifyJBosswebDirectory(config.local.jbossweb);
+        
         grunt.log.ok( 'local config' + config.local.webapp );
     });
+    
+    function verifyJBosswebDirectory(jbossweb) {
+        if (!grunt.file.exists(jbossweb)) {
+            grunt.fatal('jbossweb directory ' + jbossweb + ' configured in ./local-config.json does not exist, please deploy the exploded WAR first');
+        }
+        if (!grunt.file.isDir(jbossweb)) {
+            grunt.fatal('jbossweb directory ' + jbossweb + ' configured in ./local-config.json is not directory (must be exploded WAR)');
+        }
+        if (!grunt.file.exists(jbossweb + '/WEB-INF') || !grunt.file.isDir(jbossweb + '/WEB-INF')) {
+            grunt.fatal('jbossweb directory ' + jbossweb + ' configured in ./local-config.json does not contain directory ./WEB-INF (is it really an exploded WAR?)');
+        }
+        if (!/\/standalone\/deployments\//.test(jbossweb)) {
+            grunt.fatal('jbossweb directory ' + jbossweb + ' configured in ./local-config.json does not have parent directories /standalone/deployments/, are you sure it is a correct path to the deployed exploded WAR?');
+        }
+    }
 
 
 };
