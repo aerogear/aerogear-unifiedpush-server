5688b2b06e1ebea45fa0d1099e939d89d0ebfbb9
Bruno Oliveira <bruno@abstractj.org>
2014-07-29 11:32:10 -0300

    Update the method signature

--- servers/auth-server/src/main/java/org/jboss/aerogear/unifiedpush/keycloak/UpsKeycloakApplication.java
+++ servers/auth-server/src/main/java/org/jboss/aerogear/unifiedpush/keycloak/UpsKeycloakApplication.java
@@ -47,7 +47,7 @@ public class UpsKeycloakApplication extends KeycloakApplication {
             RealmManager manager = new RealmManager(session);
             RealmModel master = manager.getKeycloakAdminstrationRealm();
             UserModel admin = session.users().getUserByUsername("admin", master);
-            if (admin != null) session.users().removeUser(master, admin.getUsername());
+            if (admin != null) session.users().removeUser(master, admin);
             session.getTransaction().commit();
         } finally {
             session.close();
