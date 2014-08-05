19694eec2c7e8f9e5f4a624f770e3278b5e26964
Erik Jan de Wit <erikjan.dewit@gmail.com>
2014-08-05 10:36:08 +0200

    fixed group by

--- model/jpa/src/main/java/org/jboss/aerogear/unifiedpush/jpa/dao/impl/JPAPushApplicationDao.java
+++ model/jpa/src/main/java/org/jboss/aerogear/unifiedpush/jpa/dao/impl/JPAPushApplicationDao.java
@@ -96,7 +96,7 @@ public class JPAPushApplicationDao extends JPABaseDao implements PushApplication
     public Map<String, Long> countInstallationsByType(String pushApplicationID) {
         final String jpql = "select v.type, v.variantID, count(*) from Installation i join i.variant v where i.variant.variantID in "
                 + "(select v.variantID from PushApplication pa join pa.variants v where pushApplicationID = :pushApplicationID) "
-                + "group by v.variantID";
+                + "group by v.type, v.variantID";
 
         final HashMap<String, Long> results = new HashMap<String, Long>();
 
