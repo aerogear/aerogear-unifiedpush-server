96a7b1a246a0e8faaebbe671a75461de3fc39aa7
Sebastien Blanc <scm.blanc@gmail.com>
2014-08-06 11:12:53 +0200

    add a new test

--- model/jpa/src/test/java/org/jboss/aerogear/unifiedpush/jpa/PushMessageInformationDaoTest.java
+++ model/jpa/src/test/java/org/jboss/aerogear/unifiedpush/jpa/PushMessageInformationDaoTest.java
@@ -352,6 +352,80 @@ public class PushMessageInformationDaoTest {
     }
 
     @Test
+    public void findMostBusyVariantsForOnlyTwo() {
+        pushMessageInformation = pushMessageInformationDao.find(pushMessageInformationID);
+
+        final String loginName = "admin";
+
+        final PushApplication pushAppInitial = new PushApplication();
+        pushAppInitial.setName("app");
+        pushAppInitial.setPushApplicationID("231231231");
+        pushAppInitial.setDeveloper(loginName);
+        entityManager.persist(pushAppInitial);
+
+        VariantMetricInformation variantOne = new VariantMetricInformation();
+        variantOne.setDeliveryStatus(Boolean.FALSE);
+        variantOne.setReceivers(200);
+        variantOne.setVariantID("231543432432");
+        pushMessageInformation.getVariantInformations().add(variantOne);
+        pushMessageInformationDao.update(pushMessageInformation);
+
+        VariantMetricInformation variantThree = new VariantMetricInformation();
+        variantThree.setDeliveryStatus(Boolean.FALSE);
+        variantThree.setReceivers(300);
+        variantThree.setVariantID("23154343243333");
+        pushMessageInformation.getVariantInformations().add(variantThree);
+        pushMessageInformationDao.update(pushMessageInformation);
+
+        VariantMetricInformation variantFour = new VariantMetricInformation();
+        variantFour.setDeliveryStatus(Boolean.FALSE);
+        variantFour.setReceivers(1000);
+        variantFour.setVariantID("231543432434");
+        pushMessageInformation.getVariantInformations().add(variantFour);
+        pushMessageInformationDao.update(pushMessageInformation);
+
+        final PushApplication pushApp = new PushApplication();
+        pushApp.setName("app");
+        pushApp.setPushApplicationID("231231232");
+        pushApp.setDeveloper(loginName);
+        entityManager.persist(pushApp);
+
+        PushMessageInformation pmi = new PushMessageInformation();
+        pmi.setPushApplicationId("231231232");
+        pushMessageInformationDao.create(pmi);
+        VariantMetricInformation variantTwo = new VariantMetricInformation();
+        variantTwo.setDeliveryStatus(Boolean.TRUE);
+        variantTwo.setReceivers(2000);
+        variantTwo.setVariantID("231543432432");
+        pmi.getVariantInformations().add(variantTwo);
+        pushMessageInformationDao.update(pmi);
+
+        final AndroidVariant androidVariant = new AndroidVariant();
+        androidVariant.setGoogleKey("123");
+        androidVariant.setVariantID("231543432432");
+        androidVariant.setDeveloper(loginName);
+        entityManager.persist(androidVariant);
+
+        final AndroidVariant androidVariant1 = new AndroidVariant();
+        androidVariant1.setGoogleKey("123");
+        androidVariant1.setVariantID("23154343243333");
+        androidVariant1.setDeveloper(loginName);
+        entityManager.persist(androidVariant1);
+
+        final AndroidVariant androidVariant2 = new AndroidVariant();
+        androidVariant2.setGoogleKey("123");
+        androidVariant2.setVariantID("231543432434");
+        androidVariant2.setDeveloper(loginName);
+        entityManager.persist(androidVariant2);
+
+        flushAndClear();
+
+        List<PushMessageInformation> lastActivity = pushMessageInformationDao.findLastThreeActivity(loginName);
+        assertThat(lastActivity).hasSize(2);
+
+    }
+
+    @Test
     public void findVariantIDsWithWarnings() {
 
         pushMessageInformation = pushMessageInformationDao.find(pushMessageInformationID);
