45558a43622a7334b8213a6b8be531a78460df68
Bruno Oliveira <bruno@abstractj.org>
2014-10-17 14:05:23 -0300

    Inclusion of producer to provide in a single class the current logged in username

---
+++ service/src/main/java/org/jboss/aerogear/unifiedpush/service/annotations/LoggedIn.java
@@ -0,0 +1,16 @@
+package org.jboss.aerogear.unifiedpush.service.annotations;
+
+import javax.inject.Qualifier;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Current logged in user
+ */
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface LoggedIn {
+}
