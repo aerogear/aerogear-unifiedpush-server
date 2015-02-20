2742518121df64ebde45056873b212ac4145311a
Douglas Campos <qmx@qmx.me>
2015-02-19 22:01:52 -0200

    migrate certificate data

---
+++ migrator/src/main/java/org/jboss/aerogear/unifiedpush/migrator/CertificateBlobToBase64.java
@@ -0,0 +1,83 @@
+package org.jboss.aerogear.unifiedpush.migrator;
+
+import liquibase.change.custom.CustomSqlChange;
+import liquibase.database.Database;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.CustomChangeException;
+import liquibase.exception.SetupException;
+import liquibase.exception.ValidationErrors;
+import liquibase.resource.ResourceAccessor;
+import liquibase.statement.SqlStatement;
+import liquibase.statement.core.UpdateStatement;
+import net.iharder.Base64;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CertificateBlobToBase64 implements CustomSqlChange {
+    private String confirmationMessage = "";
+
+    @Override
+    public SqlStatement[] generateStatements(Database database) throws CustomChangeException {
+        List<SqlStatement> statements = new ArrayList<SqlStatement>();
+
+        Connection conn = ((JdbcConnection) (database.getConnection())).getWrappedConnection();
+
+        try {
+            conn.setAutoCommit(false);
+            ResultSet resultSet = conn.createStatement().executeQuery("SELECT id, certificate from ios_variant");
+            while (resultSet.next()) {
+                String id = resultSet.getString("id");
+                InputStream certificate = resultSet.getBinaryStream("certificate");
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                int bytesRead = -1;
+                byte[] buffer = new byte[1024];
+                while ((bytesRead = certificate.read(buffer)) != -1) {
+                    stream.write(buffer, 0, bytesRead);
+                }
+                String certificateData = Base64.encodeBytes(stream.toByteArray());
+
+                UpdateStatement updateStatement = new UpdateStatement(null, null, "ios_variant")
+                        .addNewColumnValue("cert_data", certificateData)
+                        .setWhereClause("id='" + id + "'");
+                System.out.println(updateStatement.toString());
+                statements.add(updateStatement);
+
+            }
+            conn.commit();
+
+            if (!statements.isEmpty()) {
+                confirmationMessage = "updated certificate data successfully";
+            }
+
+            return statements.toArray(new SqlStatement[statements.size()]);
+        } catch (Exception e) {
+            throw new CustomChangeException("Failed to migrate certificate data");
+        }
+
+    }
+
+    @Override
+    public String getConfirmationMessage() {
+        return confirmationMessage;
+    }
+
+    @Override
+    public void setUp() throws SetupException {
+
+    }
+
+    @Override
+    public void setFileOpener(ResourceAccessor resourceAccessor) {
+
+    }
+
+    @Override
+    public ValidationErrors validate(Database database) {
+        return null;
+    }
+}
