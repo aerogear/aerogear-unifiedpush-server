df9feb42e18efed339b7e1fb1100e7d766efe9b5
Erik Jan de Wit <erikjan.dewit@gmail.com>
2015-04-02 09:38:29 +0200

    added missing javadoc

--- model/jpa/src/main/java/org/jboss/aerogear/unifiedpush/jpa/dao/impl/JPAHealthDao.java
+++ model/jpa/src/main/java/org/jboss/aerogear/unifiedpush/jpa/dao/impl/JPAHealthDao.java
@@ -1,8 +1,27 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright Red Hat, Inc., and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.aerogear.unifiedpush.jpa.dao.impl;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
+/**
+ * Simple dao that checks if the database is available by running select 1 query.
+ */
 public class JPAHealthDao {
     @Inject
     private EntityManager entityManager;
