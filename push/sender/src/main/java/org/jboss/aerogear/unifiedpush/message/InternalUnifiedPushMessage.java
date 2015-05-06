701292c63db9ed6054c78a7319aa6174e345881b
Matthias Wessendorf <matzew@apache.org>
2015-05-06 14:28:39 +0200

    java doc cleanup

--- push/sender/src/main/java/org/jboss/aerogear/unifiedpush/message/InternalUnifiedPushMessage.java
+++ push/sender/src/main/java/org/jboss/aerogear/unifiedpush/message/InternalUnifiedPushMessage.java
@@ -19,11 +19,6 @@ package org.jboss.aerogear.unifiedpush.message;
 /**
  * This is {@link UnifiedPushMessage} with some additional information, which we will use for internal purposes,
  * like analytics tracking.
- *
- * For more information see
- * <a href="https://issues.jboss.org/browse/AGPUSH-1381">https://issues.jboss.org/browse/AGPUSH-1381</a> and
- * <a href="https://github.com/aerogear/aerogear-unifiedpush-server/pull/526#discussion-diff-28432730">
- *     https://github.com/aerogear/aerogear-unifiedpush-server/pull/526#discussion-diff-28432730</a>
  */
 public class InternalUnifiedPushMessage extends UnifiedPushMessage {
 
@@ -38,7 +33,7 @@ public class InternalUnifiedPushMessage extends UnifiedPushMessage {
     /**
      * The IP address from the agent that did issue the push message request.
      *
-     * @retun the IP address
+     * @return the IP address
      */
     public String getIpAddress() {
         return ipAddress;
