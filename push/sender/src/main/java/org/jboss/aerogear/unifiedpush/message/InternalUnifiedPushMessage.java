6c137dc17f73499d48a3305939d602a9d6dde83a
Matthias Wessendorf <matzew@apache.org>
2015-05-06 13:05:39 +0200

    Fixing javadoc and license header

--- push/sender/src/main/java/org/jboss/aerogear/unifiedpush/message/InternalUnifiedPushMessage.java
+++ push/sender/src/main/java/org/jboss/aerogear/unifiedpush/message/InternalUnifiedPushMessage.java
@@ -1,3 +1,19 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright Red Hat, Inc., and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.aerogear.unifiedpush.message;
 
 /**
@@ -16,6 +32,8 @@ public class InternalUnifiedPushMessage extends UnifiedPushMessage {
 
     /**
      * The IP address from the agent that did issue the push message request.
+     *
+     * @retun the IP address
      */
     public String getIpAddress() {
         return ipAddress;
@@ -27,6 +45,8 @@ public class InternalUnifiedPushMessage extends UnifiedPushMessage {
 
     /**
      * The Client Identifier showing who triggered the Push Notification.
+     *
+     * @return client identifier string
      */
     public String getClientIdentifier() {
         return clientIdentifier;
