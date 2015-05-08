a4e94fb12fc13599b40286b6256720125377918a
Lucas Holmquist <lholmqui@redhat.com>
2015-05-08 13:41:57 -0400

    Update node.js sender snippet

--- admin-ui/app/snippets/senders/sender-nodejs.js
+++ admin-ui/app/snippets/senders/sender-nodejs.js
@@ -3,6 +3,14 @@ var agSender = require( "unifiedpush-node-sender" ),
     url: "{{ contextPath }}",
     applicationId: "{{ app.pushApplicationID }}",
     masterSecret: "{{ app.masterSecret }}"
+  },
+  message = {
+    alert: "Hello from the Node.js Sender API!"
+  },
+  options = {
+    config: {
+        ttl: 3600,
+    }
   };
 
 agSender.Sender( settings ).send( message, options ).on( "success", function( response ) {
