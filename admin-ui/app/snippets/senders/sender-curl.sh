2973d716b6831ef5f23b71fbf9a5e12d618882ed
Matthias Wessendorf <matzew@apache.org>
2015-05-08 09:19:26 +0200

    Updating CURL / Java Senders

--- admin-ui/app/snippets/senders/sender-curl.sh
+++ admin-ui/app/snippets/senders/sender-curl.sh
@@ -2,21 +2,41 @@ curl -u "{{ app.pushApplicationID }}:{{ app.masterSecret }}"
    -v -H "Accept: application/json" -H "Content-type: application/json"
    -X POST
    -d '{
-     "alias" : ["someUsername"],
-     "deviceType" : ["someDevice"],
-     "categories" : ["someCategories"],
-     "variants" : ["someVariantIDs"],
-     "ttl" : 3600,
-     "message":
-     {
-       "key":"value",
-       "key2":"other value",
-       "alert":"HELLO!",
-       "action-category":"some value",
-       "sound":"default",
-       "badge":2,
-       "content-available" : true
+     "message": {
+      "alert": "HELLO!",
+      "sound": "default",
+      "badge": 2,
+      "user-data": {
+          "key": "value",
+          "key2": "other value"
+      },
+      "windows": {
+          "type": "tile",
+          "duration": "short",
+          "badge": "alert",
+          "tileType": "TileWideBlockAndText01",
+          "images": ["Assets/test.jpg", "Assets/background.png"],
+          "textFields": ["foreground text"]
+      },
+      "apns": {
+          "title" : "someTitle",
+          "action-category": "some value",
+          "content-available": true,
+          "action" : "someAction",
+          "url-args" :["args1","arg2"],
+          "localized-title-key" : "some value",
+          "localized-title-arguments" : ["args1","arg2"]
+      }
+      "simple-push": "version=123"
      },
-     "simple-push":"version=123"
+     "criteria": {
+         "alias": [ "someUsername" ],
+         "deviceType": [ "someDevice" ],
+         "categories": [ "someCategories" ],
+         "variants": [ "someVariantIDs" ]
+     },
+     "config": {
+         "ttl": 3600
+     }
    }'
    {{ contextPath }}rest/sender
