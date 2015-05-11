49dee8686e6c7b674bd08e610d6beeb4cb361de8
Matthias Wessendorf <matzew@apache.org>
2015-05-11 10:44:50 +0200

    Escaping and shrinking curl example to the minimum

--- admin-ui/app/snippets/senders/sender-curl.sh
+++ admin-ui/app/snippets/senders/sender-curl.sh
@@ -1,42 +1,10 @@
-curl -u "{{ app.pushApplicationID }}:{{ app.masterSecret }}"
-   -v -H "Accept: application/json" -H "Content-type: application/json"
-   -X POST
-   -d '{
+curl -u "{{ app.pushApplicationID }}:{{ app.masterSecret }}"  \
+   -v -H "Accept: application/json" -H "Content-type: application/json"  \
+   -X POST  -d \
+  '{
      "message": {
-      "alert": "HELLO!",
-      "sound": "default",
-      "badge": 2,
-      "user-data": {
-          "key": "value",
-          "key2": "other value"
-      },
-      "windows": {
-          "type": "tile",
-          "duration": "short",
-          "badge": "alert",
-          "tileType": "TileWideBlockAndText01",
-          "images": ["Assets/test.jpg", "Assets/background.png"],
-          "textFields": ["foreground text"]
-      },
-      "apns": {
-          "title" : "someTitle",
-          "action-category": "some value",
-          "content-available": true,
-          "action" : "someAction",
-          "url-args" :["args1","arg2"],
-          "localized-title-key" : "some value",
-          "localized-title-arguments" : ["args1","arg2"]
-      }
-      "simple-push": "version=123"
-     },
-     "criteria": {
-         "alias": [ "someUsername" ],
-         "deviceType": [ "someDevice" ],
-         "categories": [ "someCategories" ],
-         "variants": [ "someVariantIDs" ]
-     },
-     "config": {
-         "ttl": 3600
+      "alert": "Hello from the curl HTTP Sender!",
+      "sound": "default"
      }
-   }'
+   }'  \
    {{ contextPath }}rest/sender
