9b237280cbc73fc8a18e09090e463406d906b843
Matthias Wessendorf <matzew@apache.org>
2015-05-29 10:24:46 +0200

    removing duplicated info

--- jaxrs/src/main/java/org/jboss/aerogear/unifiedpush/rest/sender/PushNotificationSenderEndpoint.java
+++ jaxrs/src/main/java/org/jboss/aerogear/unifiedpush/rest/sender/PushNotificationSenderEndpoint.java
@@ -52,7 +52,7 @@ public class PushNotificationSenderEndpoint {
      * The Endpoint is protected using <code>HTTP Basic</code> (credentials <code>PushApplicationID:masterSecret</code>).
      * <p/><p/>
      *
-     * Messages are submitted as flexible JSON maps, like:
+     * Messages are submitted as flexible JSON maps. Below is a simple example:
      * <pre>
      * curl -u "PushApplicationID:MasterSecret"
      *   -v -H "Accept: application/json" -H "Content-type: application/json"
@@ -61,39 +61,9 @@ public class PushNotificationSenderEndpoint {
      *     "message": {
      *      "alert": "HELLO!",
      *      "sound": "default",
-     *      "badge": 2,
      *      "user-data": {
      *          "key": "value",
-     *          "key2": "other value"
-     *      },
-     *      "windows": {
-     *          "type": "tile",
-     *          "duration": "short",
-     *          "badge": "alert",
-     *          "tileType": "TileWideBlockAndText01",
-     *          "images": ["Assets/test.jpg", "Assets/background.png"],
-     *          "textFields": ["foreground text"]
-     *      },
-     *      "apns": {
-     *          "title" : "someTitle",
-     *          "action-category": "some value",
-     *          "content-available": true,
-     *          "action" : "someAction",
-     *          "url-args" :["args1","arg2"],
-     *          "localized-title-key" : "some value",
-     *          "localized-title-arguments" : ["args1","arg2"]
      *      }
-     *      "simple-push": "version=123"
-     *     },
-     *     "criteria": {
-     *         "alias": [ "someUsername" ],
-     *         "deviceType": [ "someDevice" ],
-     *         "categories": [ "someCategories" ],
-     *         "variants": [ "someVariantIDs" ]
-     *     },
-     *     "config": {
-     *         "ttl": 3600
-     *     }
      *   }'
      *   https://SERVER:PORT/CONTEXT/rest/sender
      * </pre>
