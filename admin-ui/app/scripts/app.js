275b7d6762b7550ead15366afe764f726a70610e
Erik Jan de Wit <erikjan.dewit@gmail.com>
2015-06-19 12:12:27 +0200

    move keycloak specifics to separate file that is not included into modules or scripts

--- admin-ui/app/scripts/app.js
+++ admin-ui/app/scripts/app.js
@@ -15,40 +15,9 @@
     'ngClipboard'
   ]);
 
-  /**
-   * Snippet extracted from Keycloak examples
-   */
-  var auth = {};
-
-  angular.element(document).ready(function () {
-    var keycloak = new Keycloak('config/admin-ui-keycloak.json');
-    auth.loggedIn = false;
-
-    keycloak.init({ onLoad: 'login-required' }).success(function () {
-      auth.loggedIn = true;
-      auth.keycloak = keycloak;
-      auth.logout = function() {
-        auth.loggedIn = false;
-        auth.keycloak = null;
-        window.location = keycloak.authServerUrl + '/realms/aerogear/tokens/logout?redirect_uri=' + window.location.href;
-      };
-      app.factory('Auth', function () {
-        return auth;
-      });
-      angular.bootstrap(document, ['upsConsole']);
-    }).error(function () {
-      window.location.reload();
-    });
-
-  });
-
   app.run(function($rootScope) {
     // allow to retrieve $rootScope in views (for clarification of access scope)
     $rootScope.$rootScope = $rootScope;
-  });
-
-  app.factory('Auth', function () {
-    return auth;
   });
 
   var appConfig = {
@@ -69,34 +38,6 @@
         return appConfig;
       }
     };
-  });
-
-  app.factory('authInterceptor', function ($q, Auth) {
-    return {
-      request: function (config) {
-        var deferred = $q.defer();
-
-        if (config.url === 'rest/sender' || config.url === 'rest/registry/device/importer') {
-          return config;
-        }
-
-        if (Auth.keycloak && Auth.keycloak.token) {
-          Auth.keycloak.updateToken(5).success(function () {
-            config.headers = config.headers || {};
-            config.headers.Authorization = 'Bearer ' + Auth.keycloak.token;
-
-            deferred.resolve(config);
-          }).error(function () {
-            window.location.reload();
-          });
-        }
-        return deferred.promise;
-      }
-    };
-  });
-
-  app.config(function ($httpProvider) {
-    $httpProvider.interceptors.push('authInterceptor');
   });
 
   app.config(function ($logProvider, appConfigProvider) {
