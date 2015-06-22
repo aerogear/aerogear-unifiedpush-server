5cbc0e7d116bdff3c7a618fb31e1dbdc0609ee8b
yanivn <yanivn@c-b4.local>
2015-06-22 10:12:32 +0300

    Add resources to rpm build

--- build/files/install/bin/redhat/install.sh
+++ build/files/install/bin/redhat/install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+get_full_path() {
+    # Absolute path to this script, e.g. /home/user/bin/foo.sh
+    SCRIPT=$(readlink -f $0)
+
+    if [ ! -d ${SCRIPT} ]; then
+        # Absolute path this script is in, thus /home/user/bin
+        SCRIPT=`dirname $SCRIPT`
+    fi
+
+    ( cd "${SCRIPT}" ; pwd )
+}
+
 install_files() {
 	local base_dir="$1"
 
--- jaxrs/src/test/java/org/jboss/aerogear/unifiedpush/rest/registry/applications/InstallationManagementEndpointTest.java
+++ jaxrs/src/test/java/org/jboss/aerogear/unifiedpush/rest/registry/applications/InstallationManagementEndpointTest.java
@@ -1,84 +1,82 @@
 package org.jboss.aerogear.unifiedpush.rest.registry.applications;
 
-import org.jboss.resteasy.specimpl.UriInfoImpl;
-import org.jboss.resteasy.spi.Link;
-import org.jboss.resteasy.spi.LinkHeader;
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import javax.ws.rs.core.PathSegment;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Collections;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.jboss.resteasy.spi.Link;
+import org.jboss.resteasy.spi.LinkHeader;
+import org.jboss.resteasy.spi.ResteasyUriInfo;
+import org.junit.Test;
 
 /**
  */
 public class InstallationManagementEndpointTest {
 
-    private final InstallationManagementEndpoint endpoint = new InstallationManagementEndpoint();
+	private final InstallationManagementEndpoint endpoint = new InstallationManagementEndpoint();
 
-    @Test
-    public void shouldGenerateHeaderLinksFirstPage() throws URISyntaxException {
-        //given
-        final UriInfoImpl uriInfo = getUriInfo();
+	@Test
+	public void shouldGenerateHeaderLinksFirstPage() throws URISyntaxException {
+		// given
+		final ResteasyUriInfo uriInfo = getUriInfo();
 
-        //when
-        final LinkHeader linkHeader = endpoint.getLinkHeader(0, 3, uriInfo);
+		// when
+		final LinkHeader linkHeader = endpoint.getLinkHeader(0, 3, uriInfo);
 
-        //then
-        assertThat(findLinkByRel(linkHeader, "prev")).isNull();
-        final Link next = findLinkByRel(linkHeader, "next");
-        assertThat(next).isNotNull();
-        assertThat(next.getHref()).isEqualTo("/?page=1");
-        assertThat(findLinkByRel(linkHeader, "first")).isNull();
-    }
+		// then
+		assertThat(findLinkByRel(linkHeader, "prev")).isNull();
+		final Link next = findLinkByRel(linkHeader, "next");
+		assertThat(next).isNotNull();
+		assertThat(next.getHref()).isEqualTo("/?page=1");
+		assertThat(findLinkByRel(linkHeader, "first")).isNull();
+	}
 
-    @Test
-    public void shouldGenerateHeaderLinksNormalPage() throws URISyntaxException {
-        //given
-        final UriInfoImpl uriInfo = getUriInfo();
+	@Test
+	public void shouldGenerateHeaderLinksNormalPage() throws URISyntaxException {
+		// given
+		final ResteasyUriInfo uriInfo = getUriInfo();
 
-        //when
-        final LinkHeader linkHeader = endpoint.getLinkHeader(2, 3, uriInfo);
+		// when
+		final LinkHeader linkHeader = endpoint.getLinkHeader(2, 3, uriInfo);
 
-        final Link prev = findLinkByRel(linkHeader, "prev");
-        assertThat(prev).isNotNull();
-        assertThat(prev.getHref()).isEqualTo("/?page=1");
-        final Link next = findLinkByRel(linkHeader, "next");
-        assertThat(next).isNotNull();
-        assertThat(next.getHref()).isEqualTo("/?page=3");
+		final Link prev = findLinkByRel(linkHeader, "prev");
+		assertThat(prev).isNotNull();
+		assertThat(prev.getHref()).isEqualTo("/?page=1");
+		final Link next = findLinkByRel(linkHeader, "next");
+		assertThat(next).isNotNull();
+		assertThat(next.getHref()).isEqualTo("/?page=3");
 
-    }
+	}
 
-    @Test
-    public void shouldGenerateHeaderLinksLastPage() throws URISyntaxException {
-        //given
-        final UriInfoImpl uriInfo = getUriInfo();
+	@Test
+	public void shouldGenerateHeaderLinksLastPage() throws URISyntaxException {
+		// given
+		final ResteasyUriInfo uriInfo = getUriInfo();
 
-        //when
-        final LinkHeader linkHeader = endpoint.getLinkHeader(3, 3, uriInfo);
+		// when
+		final LinkHeader linkHeader = endpoint.getLinkHeader(3, 3, uriInfo);
 
-        final Link prev = findLinkByRel(linkHeader, "prev");
-        assertThat(prev).isNotNull();
-        assertThat(prev.getHref()).isEqualTo("/?page=2");
-        final Link first = findLinkByRel(linkHeader, "first");
-        assertThat(first).isNotNull();
-        assertThat(first.getHref()).isEqualTo("/?page=0");
-        assertThat(findLinkByRel(linkHeader, "last")).isNull();
-    }
+		final Link prev = findLinkByRel(linkHeader, "prev");
+		assertThat(prev).isNotNull();
+		assertThat(prev.getHref()).isEqualTo("/?page=2");
+		final Link first = findLinkByRel(linkHeader, "first");
+		assertThat(first).isNotNull();
+		assertThat(first.getHref()).isEqualTo("/?page=0");
+		assertThat(findLinkByRel(linkHeader, "last")).isNull();
+	}
 
-    private Link findLinkByRel(LinkHeader linkHeader, String rel) {
-        for (Link link : linkHeader.getLinks()) {
-            if (link.getRelationship().equals(rel)) {
-                return link;
-            }
-        }
-        return null;
+	private Link findLinkByRel(LinkHeader linkHeader, String rel) {
+		for (Link link : linkHeader.getLinks()) {
+			if (link.getRelationship().equals(rel)) {
+				return link;
+			}
+		}
+		return null;
 
-    }
+	}
 
-    private UriInfoImpl getUriInfo() throws URISyntaxException {
-        return new UriInfoImpl(new URI("/"), new URI("http://localhost"), "/", "", Collections.<PathSegment>emptyList());
-    }
+	private ResteasyUriInfo getUriInfo() throws URISyntaxException {
+		return new ResteasyUriInfo(new URI("/"), new URI("http://localhost"));
+	}
 }
