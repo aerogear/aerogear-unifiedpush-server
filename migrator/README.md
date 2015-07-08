a2972d62bdfc145f9aa22e4fbef0ba621837feb4
Douglas Campos <qmx@qmx.me>
2015-07-08 13:00:16 -0300

    bring vanilla migrator from stable

---
+++ migrator/README.md
@@ -0,0 +1,63 @@
+# UnifiedPush Server - Migration guide
+
+For the database migration you may need to run a simple shell script.
+
+# 1.0.x users
+
+For the migration to our 1.0.3 release we are only supporting the 1.0.x series of the UnifiedPush Server:
+
+* 1.0.0
+* 1.0.1
+* 1.0.2
+
+In terms of supported databases, we are supporting MySQL 5.5 and PostgreSQL 9. 
+
+## Getting started
+
+Extract the `unifiedpush-migrator-dist.zip` file to your desired location. 
+
+_NOTE:_ It is recommended to shutdown the application server, while performing the migration.
+
+### Database Migration
+
+Below you find information on how to perform the migration on each of the supported databases.
+
+#### PostgreSQL
+
+Inside of the extracted zip file, there is a `liquibase-postgresql-example.properties`, copy that to `liquibase.properties`.
+
+    cp liquibase-postgresql-example.properties liquibase.properties
+
+Once done, you need to edit the new file to match your database name and credentials.
+
+#### MySQL
+
+Inside of the extracted zip file, there is a `liquibase-mysql-example.properties`, copy that to `liquibase.properties`.
+
+    cp liquibase-mysql-example.properties liquibase.properties
+
+Once done, you need to edit the new file to match your database name and credentials.
+
+#### Run the script
+
+After the `liquibase.properties` contains the proper credentials, you need to execute the migration tool:
+
+    ./bin/ups-migrator update
+
+In case of a successful run, the script prints
+
+    Liquibase Update Successful
+
+
+If you want to get some more details on the actual migration steps, enable logging while performing the database migration:
+
+    ./bin/ups-migrator --logLevel=DEBUG update
+
+
+### WAR migration
+
+After successfully executing the above database migration script you need to replace the deployed 1.0.x WAR files with the new ones and start the application server.
+
+That's it :smile:
+
+In case of any error, please contact us on our [mailing list](https://github.com/aerogear/aerogear-unifiedpush-server#project-info)!
---
+++ migrator/src/main/java/org/jboss/aerogear/unifiedpush/migrator/CertificateBlobToBase64.java
@@ -0,0 +1,100 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright Red Hat, Inc., and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.aerogear.unifiedpush.migrator;
+
+import liquibase.change.custom.CustomSqlChange;
+import liquibase.database.Database;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.CustomChangeException;
+import liquibase.exception.SetupException;
+import liquibase.exception.ValidationErrors;
+import liquibase.resource.ResourceAccessor;
+import liquibase.statement.SqlStatement;
+import liquibase.statement.core.UpdateStatement;
+import net.iharder.Base64;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CertificateBlobToBase64 implements CustomSqlChange {
+    private String confirmationMessage = "";
+
+    @Override
+    public SqlStatement[] generateStatements(Database database) throws CustomChangeException {
+        List<SqlStatement> statements = new ArrayList<SqlStatement>();
+
+        Connection conn = ((JdbcConnection) (database.getConnection())).getWrappedConnection();
+
+        try {
+            conn.setAutoCommit(false);
+            ResultSet resultSet = conn.createStatement().executeQuery("SELECT id, certificate from ios_variant");
+            while (resultSet.next()) {
+                String id = resultSet.getString("id");
+                Blob blob = resultSet.getBlob("certificate");
+                InputStream certificate = blob.getBinaryStream();
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                int bytesRead = -1;
+                byte[] buffer = new byte[1024];
+                while ((bytesRead = certificate.read(buffer)) != -1) {
+                    stream.write(buffer, 0, bytesRead);
+                }
+                String certificateData = Base64.encodeBytes(stream.toByteArray());
+
+                UpdateStatement updateStatement = new UpdateStatement(null, null, "ios_variant")
+                        .addNewColumnValue("cert_data", certificateData)
+                        .setWhereClause("id='" + id + "'");
+                statements.add(updateStatement);
+
+            }
+            conn.commit();
+
+            if (!statements.isEmpty()) {
+                confirmationMessage = "updated certificate data successfully";
+            }
+
+            return statements.toArray(new SqlStatement[statements.size()]);
+        } catch (Exception e) {
+            throw new CustomChangeException("Failed to migrate certificate data");
+        }
+
+    }
+
+    @Override
+    public String getConfirmationMessage() {
+        return confirmationMessage;
+    }
+
+    @Override
+    public void setUp() throws SetupException {
+
+    }
+
+    @Override
+    public void setFileOpener(ResourceAccessor resourceAccessor) {
+
+    }
+
+    @Override
+    public ValidationErrors validate(Database database) {
+        return null;
+    }
+}
