96f3862ab0f73553982b4ea0473431b0425fb681
Daniel Bevenius <daniel.bevenius@gmail.com>
2015-08-13 11:52:14 +0200

    Revert "Updating JPSInstallationDao to use variant.id instead of variantID"
    This reverts commit 75ff5cdad7ca1561ab62ccd5ac3bf0a0b0826c5d.

--- service/src/main/java/org/jboss/aerogear/unifiedpush/service/impl/ClientInstallationServiceImpl.java
+++ service/src/main/java/org/jboss/aerogear/unifiedpush/service/impl/ClientInstallationServiceImpl.java
@@ -60,7 +60,7 @@ public class ClientInstallationServiceImpl implements ClientInstallationService 
     public void addInstallation(Variant variant, Installation entity) {
 
         // does it already exist ?
-        Installation installation = this.findInstallationForVariantByDeviceToken(variant.getId(), entity.getDeviceToken());
+        Installation installation = this.findInstallationForVariantByDeviceToken(variant.getVariantID(), entity.getDeviceToken());
 
         // Needed for the Admin UI Only. Help for setting up Routes
         entity.setPlatform(variant.getType().getTypeName());
@@ -90,7 +90,7 @@ public class ClientInstallationServiceImpl implements ClientInstallationService 
             return;
         }
 
-        Set<String> existingTokens = installationDao.findAllDeviceTokenForVariantID(variant.getId());
+        Set<String> existingTokens = installationDao.findAllDeviceTokenForVariantID(variant.getVariantID());
 
         // clear out:
         installationDao.flushAndClear();
