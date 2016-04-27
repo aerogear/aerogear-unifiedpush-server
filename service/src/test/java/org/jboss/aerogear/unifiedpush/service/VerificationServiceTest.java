99a6e688315bfb7d45180d0bc7261c7ef787dff5
yaniv-cb4 <yanivn@c-b4.com>
2016-04-27 15:01:14 +0300

    Add custom PhoneValidator which validate according to google libphonenumber or digits only format

--- service/src/test/java/org/jboss/aerogear/unifiedpush/service/VerificationServiceTest.java
+++ service/src/test/java/org/jboss/aerogear/unifiedpush/service/VerificationServiceTest.java
@@ -4,10 +4,10 @@ import java.lang.annotation.Annotation;
 
 import javax.validation.ConstraintValidator;
 
-import org.hibernate.validator.internal.constraintvalidators.DigitsValidatorForCharSequence;
 import org.jboss.aerogear.unifiedpush.api.validation.AlwaysTrueValidator;
 import org.jboss.aerogear.unifiedpush.service.impl.VerificationGatewayServiceImpl;
 import org.jboss.aerogear.unifiedpush.service.sms.ClickatellSMSSender;
+import org.jboss.aerogear.unifiedpush.service.validation.PhoneValidator;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
@@ -46,16 +46,24 @@ public class VerificationServiceTest {
 		}
 	}
 
-	@SuppressWarnings("rawtypes" )
+	@SuppressWarnings({ "rawtypes", "unchecked" } )
 	@Test
 	public void singleImplWithType() {
-		System.setProperty(VerificationGatewayServiceImpl.VERIFICATION_IMPL_KEY, "org.hibernate.validator.internal.constraintvalidators.DigitsValidatorForCharSequence::org.jboss.aerogear.unifiedpush.service.sms.ClickatellSMSSender");
+		System.setProperty(VerificationGatewayServiceImpl.VERIFICATION_IMPL_KEY, "org.jboss.aerogear.unifiedpush.service.validation.PhoneValidator::org.jboss.aerogear.unifiedpush.service.sms.ClickatellSMSSender");
 
 		init();
 
 		for (ConstraintValidator validator : vService.getPublishers().keySet()) {
-			Assert.assertTrue(DigitsValidatorForCharSequence.class.isAssignableFrom(validator.getClass()));
+			Assert.assertTrue(PhoneValidator.class.isAssignableFrom(validator.getClass()));
 			Assert.assertTrue(ClickatellSMSSender.class.isAssignableFrom(vService.getPublishers().get(validator).getClass()));
+
+
+			Assert.assertTrue(validator.isValid("+13216549877", null));
+			Assert.assertTrue(validator.isValid("+0013216549877", null));
+			Assert.assertTrue(validator.isValid("0013216549877", null));
+			Assert.assertTrue(validator.isValid("443216549877", null));
+			Assert.assertTrue(validator.isValid("3216549877", null));
+			Assert.assertFalse(validator.isValid("xxx@test.com", null));
 		}
 	}
 
@@ -63,12 +71,12 @@ public class VerificationServiceTest {
 	@SuppressWarnings("rawtypes" )
 	@Test
 	public void singleImplWithType1() {
-		System.setProperty(VerificationGatewayServiceImpl.VERIFICATION_IMPL_KEY, "org.hibernate.validator.internal.constraintvalidators.DigitsValidatorForCharSequence::org.jboss.aerogear.unifiedpush.service.sms.ClickatellSMSSender;");
+		System.setProperty(VerificationGatewayServiceImpl.VERIFICATION_IMPL_KEY, "org.jboss.aerogear.unifiedpush.service.validation.PhoneValidator::org.jboss.aerogear.unifiedpush.service.sms.ClickatellSMSSender;");
 
 		init();
 
 		for (ConstraintValidator validator : vService.getPublishers().keySet()) {
-			Assert.assertTrue(DigitsValidatorForCharSequence.class.isAssignableFrom(validator.getClass()));
+			Assert.assertTrue(PhoneValidator.class.isAssignableFrom(validator.getClass()));
 			Assert.assertTrue(ClickatellSMSSender.class.isAssignableFrom(vService.getPublishers().get(validator).getClass()));
 		}
 	}
@@ -76,14 +84,14 @@ public class VerificationServiceTest {
 	@SuppressWarnings("rawtypes" )
 	@Test
 	public void multipleImplWithType() {
-		System.setProperty(VerificationGatewayServiceImpl.VERIFICATION_IMPL_KEY, "org.hibernate.validator.internal.constraintvalidators.DigitsValidatorForCharSequence::org.jboss.aerogear.unifiedpush.service.sms.ClickatellSMSSender;org.hibernate.validator.internal.constraintvalidators.DigitsValidatorForCharSequence::org.jboss.aerogear.unifiedpush.service.sms.ClickatellSMSSender");
+		System.setProperty(VerificationGatewayServiceImpl.VERIFICATION_IMPL_KEY, "org.jboss.aerogear.unifiedpush.service.validation.PhoneValidator::org.jboss.aerogear.unifiedpush.service.sms.ClickatellSMSSender;org.jboss.aerogear.unifiedpush.service.validation.PhoneValidator::org.jboss.aerogear.unifiedpush.service.sms.ClickatellSMSSender");
 
 		init();
 
 		Assert.assertTrue(vService.getPublishers().size() == 2);
 
 		for (ConstraintValidator validator : vService.getPublishers().keySet()) {
-			Assert.assertTrue(DigitsValidatorForCharSequence.class.isAssignableFrom(validator.getClass()));
+			Assert.assertTrue(PhoneValidator.class.isAssignableFrom(validator.getClass()));
 			Assert.assertTrue(ClickatellSMSSender.class.isAssignableFrom(vService.getPublishers().get(validator).getClass()));
 		}
 	}
