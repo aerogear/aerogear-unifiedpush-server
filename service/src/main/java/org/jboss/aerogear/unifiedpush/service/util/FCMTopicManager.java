49f2e87d99d280b98c424f9a17048cbe57809776
yaniv-cb4 <yanivn@c-b4.com>
2016-07-07 07:57:09 +0300

    Prevent NullPointerException when unsubscribe categoris

--- service/src/main/java/org/jboss/aerogear/unifiedpush/service/util/FCMTopicManager.java
+++ service/src/main/java/org/jboss/aerogear/unifiedpush/service/util/FCMTopicManager.java
@@ -69,6 +69,10 @@ public class FCMTopicManager {
             return new HashSet<>(0);
         }
         JSONObject rel = (JSONObject) info.get("rel");
+        if (rel == null){
+        	logger.debug("Couldn't parse rel object Instance ID service.");
+            return new HashSet<String>(0);
+        }
         JSONObject topics = (JSONObject) rel.get("topics");
         return topics.keySet();
     }
