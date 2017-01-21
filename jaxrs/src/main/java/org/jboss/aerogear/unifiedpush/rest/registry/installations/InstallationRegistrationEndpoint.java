28da9e63f21c0b2c2ef2b9133ec597cf7d3790ca
Dimitra Zuccarelli <dimitrazuccarelli@gmail.com>
2017-01-21 11:45:05 +0100

    Change properties path

--- jaxrs/src/main/java/org/jboss/aerogear/unifiedpush/rest/registry/installations/InstallationRegistrationEndpoint.java
+++ jaxrs/src/main/java/org/jboss/aerogear/unifiedpush/rest/registry/installations/InstallationRegistrationEndpoint.java
@@ -38,7 +38,16 @@ import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.OPTIONS;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
@@ -222,8 +231,7 @@ public class InstallationRegistrationEndpoint extends AbstractBaseEndpoint {
             return create401Response(request);
         }
 
-        // /resources/kafka/producer.props
-        try (InputStream props = new FileInputStream(this.getClass().getResourceAsStream("/path/to/properties")) {
+        try (InputStream props = new FileInputStream(this.getClass().getResourceAsStream("/kafka/producer.props")) {
             Properties properties = new Properties();
             properties.load(props);
 
