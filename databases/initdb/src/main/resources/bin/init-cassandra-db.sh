3528c655eb53985046e6fe93af10d301374629bc
yaniv-cb4 <yanivn@c-b4.com>
2017-03-01 08:15:51 +0200

    Change database param to keyspace

--- databases/initdb/src/main/resources/bin/init-cassandra-db.sh
+++ databases/initdb/src/main/resources/bin/init-cassandra-db.sh
@@ -15,13 +15,13 @@ die() {
         exit 1
 }
 
-database="$1"
-[ -z "${database}" ] && die "Missing database! Usage: ./init-cassandra-db.sh database name"
+keyspace="$1"
+[ -z "${keyspace}" ] && die "Missing keyspace! Usage: ./init-cassandra-db.sh keyspace-name"
 
 #remote debug parameters
 #export DEBUG_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=1044,server=y,suspend=y"
 
 # TODO - Support username password, e.g -u myusername -p mypassword localhost
-# TODO - Use database parameter in .cql files.
-cqlsh -f cassandra-keyspace.cql >> /tmp/inti-cassandra.log
-cqlsh -f cassandra-tables.cql >> /tmp/inti-cassandra.log
+# TODO - Use keyspace parameter in .cql files.
+cqlsh -f cassandra-keyspace.cql >> /tmp/init-cassandra.log
+cqlsh -f cassandra-tables.cql >> /tmp/init-cassandra.log
