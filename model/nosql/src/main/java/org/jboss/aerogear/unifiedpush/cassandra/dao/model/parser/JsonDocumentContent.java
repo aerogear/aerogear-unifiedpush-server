628e5c86b8386e5c62626368417f72d590e96366
yaniv-cb4 <yanivn@c-b4.com>
2017-05-29 10:32:24 +0300

    Parse document snapshot as UUID

--- model/nosql/src/main/java/org/jboss/aerogear/unifiedpush/cassandra/dao/model/parser/JsonDocumentContent.java
+++ model/nosql/src/main/java/org/jboss/aerogear/unifiedpush/cassandra/dao/model/parser/JsonDocumentContent.java
@@ -1,8 +1,12 @@
 package org.jboss.aerogear.unifiedpush.cassandra.dao.model.parser;
 
+import java.util.UUID;
+
 import org.jboss.aerogear.unifiedpush.cassandra.dao.impl.DocumentKey;
 import org.jboss.aerogear.unifiedpush.cassandra.dao.model.DocumentContent;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRawValue;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
@@ -10,9 +14,13 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
  * JSON wrapper parser to DocumentContent. JSON is serialized by including
  * literal String content as is, without quoting of characters.
  * <p>
+ * Clients (JAVA) might serialize using {@link IDocument} derived classes,
+ * therefore @JsonIgnoreProperties is enabled.
+ * <p>
  * Warning: the resulting JSON stream may be invalid depending on your input
  * value.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class JsonDocumentContent extends DocumentContent {
 	private static final String CONTENT_TYPE = "application/json";
 
@@ -47,5 +55,10 @@ public class JsonDocumentContent extends DocumentContent {
 			throw new UnsupportedContentTypeException(String.format("Only %s type is permitted.", CONTENT_TYPE));
 		}
 		super.setContentType(contentType);
+	}
+
+	@JsonProperty(required = false)
+	public UUID getSnapshot() {
+		return super.getKey().getSnapshot();
 	}
 }
