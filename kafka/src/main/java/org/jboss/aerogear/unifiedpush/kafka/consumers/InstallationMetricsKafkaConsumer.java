974ec23230cae61ca5454f8a500e27abe40b0944
Polina Koleva <polina@Polinas-MacBook-Pro.local>
2017-07-12 14:00:32 +0200

    Add installation metrics consumer injection [AGPUSH-2125]

--- kafka/src/main/java/org/jboss/aerogear/unifiedpush/kafka/consumers/InstallationMetricsKafkaConsumer.java
+++ kafka/src/main/java/org/jboss/aerogear/unifiedpush/kafka/consumers/InstallationMetricsKafkaConsumer.java
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * 	http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,18 +16,20 @@
  */
 package org.jboss.aerogear.unifiedpush.kafka.consumers;
 
-import java.util.Arrays;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
+import javax.inject.Inject;
+
 import org.jboss.aerogear.unifiedpush.service.metrics.PushMessageMetricsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import net.wessendorf.kafka.cdi.annotation.Consumer;
 
 /**
  * Kafka Consumer that reads from "installationMetrics" topic a pair (PushMessageID, VariantID) and updates analytics by
  * invocation of {@link PushMessageMetricsService#updateAnalytics(String, String)}.
  * 
  */
-public class InstallationMetricsKafkaConsumer extends AbstractKafkaConsumer {
+public class InstallationMetricsKafkaConsumer {
 
     private final Logger logger = LoggerFactory.getLogger(InstallationMetricsKafkaConsumer.class);
 
@@ -37,23 +39,20 @@ public class InstallationMetricsKafkaConsumer extends AbstractKafkaConsumer {
     public static final String KAFKA_INSTALLATION_TOPIC = "installationMetrics";
 
     /**
-     * Path to consumer's properties file.
+     * Consumer's groupId.
      */
-    public static final String KAFKA_CONSUMER_PROPERTIES_PATH = "/kafka/consumer.properties";
+    public static final String KAFKA_INSTALLATION_TOPIC_CONSUMER_GROUP_ID = "installationMetricsGroup";
 
-    /**
-     * Service that updates metrics analytics.
-     */
+    @Inject
     private PushMessageMetricsService metricsService;
 
-    public InstallationMetricsKafkaConsumer(PushMessageMetricsService metricsService) {
-        super(KAFKA_CONSUMER_PROPERTIES_PATH, Arrays.asList(KAFKA_INSTALLATION_TOPIC));
-        this.metricsService = metricsService;
-    }
-
-    @Override
-    public void handleRecord(ConsumerRecord<?, ?> record) {
-        logger.info("Update metric analytics for push message's ID {} and variant's ID {}", record.key(), record.value());
-        metricsService.updateAnalytics((String) record.key(), (String) record.value());
+    /**
+     * A method invoked for each record that a consumer reads. It updates metrics analytics based on push message id and variant
+     * id.
+     */
+    @Consumer(topic = KAFKA_INSTALLATION_TOPIC, groupId = KAFKA_INSTALLATION_TOPIC_CONSUMER_GROUP_ID)
+    public void receiver(final String pushMessageId, final String variantId) {
+        logger.info("Update metric analytics for push message's ID {} and variant's ID {}", pushMessageId, variantId);
+        metricsService.updateAnalytics(pushMessageId, variantId);
     }
 }
