dfecbb2016afdd013dcec6192add948a9f94b377
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2017-08-24 14:05:29 +0200

    Response is 409 when app exists

--- service/src/main/java/org/jboss/aerogear/unifiedpush/service/impl/PushApplicationServiceImpl.java
+++ service/src/main/java/org/jboss/aerogear/unifiedpush/service/impl/PushApplicationServiceImpl.java
@@ -1,13 +1,13 @@
 /**
  * JBoss, Home of Professional Open Source
  * Copyright Red Hat, Inc., and individual contributors.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- * 	http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -41,10 +41,16 @@ public class PushApplicationServiceImpl implements PushApplicationService {
     }
 
     @Override
-    public void addPushApplication(PushApplication pushApp) {
-
+    public void addPushApplication(PushApplication pushApp) throws IllegalArgumentException {
         pushApp.setDeveloper(loginName.get());
-        pushApplicationDao.create(pushApp);
+
+        final String pushApplicationID = pushApp.getPushApplicationID();
+
+        if (findByPushApplicationID(pushApplicationID) != null) {
+            throw new IllegalArgumentException("PushApplicationID exits already: " + pushApplicationID);
+        } else {
+            pushApplicationDao.create(pushApp);
+        }
     }
 
     @Override
