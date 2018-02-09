b7b736113d5c1cba2f45ae71cda9ccf06b4e7dfa
Matthias Wessendorf <matzew@apache.org>
2018-02-09 11:11:21 +0100

    Adding AG_prefix (#965)

--- swarms/core-swarm/src/main/java/org/aergear/ups/rest/sender/PushNotificationSenderEndpoint.java
+++ swarms/core-swarm/src/main/java/org/aergear/ups/rest/sender/PushNotificationSenderEndpoint.java
@@ -25,7 +25,7 @@ public class PushNotificationSenderEndpoint {
 
     private static final Logger logger = LoggerFactory.getLogger(PushNotificationSenderEndpoint.class);
     private static final Counter promPrushRequestsTotal = Counter.build()
-            .name("push_requests_total")
+            .name("aerogear_ups_push_requests_total")
             .help("Total number of push requests.")
             .register();
 
