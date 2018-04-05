07f03f49ec6c34ae14fa02d6484f541395410e54
Matthias Wessendorf <matzew@apache.org>
2018-04-05 11:07:24 +0200

    Add psql stuff master (#975)
    * Add installer script for PSQL/WF
    
    * Adding PSQL compose-file, and renaming our mysql file
    
    * Added `set -e` to exit the script on any error
    Replaced references to the `.` folder with a properly populated environment variable

---
+++ databases/prepare_clean_psql_wf11.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+SCRIPTDIR=`dirname $0`
+
+if [ ! "$WILDFLY_HOME" ]; then
+    printf "Error: Please configure the installation path for WildFly\n"
+    printf "   >>> Ex: WILDFLY_HOME=/path/to/your/wildfly11-installation\n"
+    exit 1
+else
+    printf "😂 Awesome, WildFly 11 is configured!\n"
+    printf "   >>> Preparing the PostgreSQL DB module\n"
+    cp -r $SCRIPTDIR/src/main/resources/modules/org $WILDFLY_HOME/modules
+    mvn dependency:copy -Dartifact=org.postgresql:postgresql:9.2-1004-jdbc41 -DoutputDirectory=$WILDFLY_HOME/modules/org/postgresql/main/  
+    printf "WildFly 11 DB configuration is about to start\n"
+    printf "   >>> Running the jboss-cli tool!\n"
+    $WILDFLY_HOME/bin/jboss-cli.sh --file=$SCRIPTDIR/postgresql-database-config-wildfly.cli
+
+    printf "WildFly 11 JMS configuration is about to start\n"
+    printf "   >>> Running the jboss-cli tool!\n"
+    $WILDFLY_HOME/bin/jboss-cli.sh --file=$SCRIPTDIR/../configuration/jms-setup-wildfly.cli
+
+    printf "Huzza, it worked!\n"
+
+fi
+exit 0
