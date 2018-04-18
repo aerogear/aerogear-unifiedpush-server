02871885ac31abfc0973dbf48ad2529175e3bc7b
Matthias Wessendorf <matzew@apache.org>
2018-04-18 15:35:02 +0200

    Installation (#988)
    * removing outdated 1.0 migration scripts
    
    * Rename scripts to not imply WF11 support only
    
    * Remove H2 deployments, those are now in docker
    
    * standalone-ha was never supported
    
    * Rename mysql script
    
    * Adding standalone KC file
    
    * Some installation docs

--- databases/migration/keycloak-migration-postgresql.sql
+++
@@ -1,6 +0,0 @@
---
--- Migrate KC refactorings done between 1.0-beta-4 and 1.0.Final
---
-alter table REALM_AUDIT_LISTENERS rename to REALM_EVENTS_LISTENERS;
-alter table REALM rename column AUDIT_ENABLED to EVENTS_ENABLED;
-alter table REALM rename column AUDIT_EXPIRATION to EVENTS_EXPIRATION;
