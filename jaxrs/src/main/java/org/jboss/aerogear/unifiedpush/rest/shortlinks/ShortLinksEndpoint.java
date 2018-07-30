d23a63a2c162fb169944a27c8f5f038e472ef37c
Yaniv Marom Nachumi <yanivn@c-b4.com>
2018-07-30 16:30:48 +0300

    Send SMS using ups

--- jaxrs/src/main/java/org/jboss/aerogear/unifiedpush/rest/shortlinks/ShortLinksEndpoint.java
+++ jaxrs/src/main/java/org/jboss/aerogear/unifiedpush/rest/shortlinks/ShortLinksEndpoint.java
@@ -30,8 +30,11 @@ import org.jboss.aerogear.unifiedpush.cassandra.dao.model.DocumentContent;
 import org.jboss.aerogear.unifiedpush.rest.AbstractEndpoint;
 import org.jboss.aerogear.unifiedpush.service.AliasService;
 import org.jboss.aerogear.unifiedpush.service.DocumentService;
+import org.jboss.aerogear.unifiedpush.service.impl.spring.IConfigurationService;
+import org.jboss.aerogear.unifiedpush.service.sms.ClickatellSMSSender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.qmino.miredot.annotations.ReturnType;
@@ -47,6 +50,10 @@ public class ShortLinksEndpoint extends AbstractEndpoint {
 	private DocumentService documentService;
 	@Inject
 	private AliasService aliasService;
+	@Autowired
+	private IConfigurationService configurationService;
+
+	private final static ClickatellSMSSender sender = new ClickatellSMSSender();
 
 	/**
 	 * Cross Origin for shortlinks requests.
@@ -104,6 +111,7 @@ public class ShortLinksEndpoint extends AbstractEndpoint {
 
 		long ttl = 3600;
 		String code = getCode(type);
+		String shortLink = getShortLink(code, request);
 
 		// TODO - Validate JWT and use expire as TTL
 		// JWT validation should be enabled/disabled using -D to allow unit test
@@ -114,32 +122,60 @@ public class ShortLinksEndpoint extends AbstractEndpoint {
 			return appendAllowOriginHeader(Response.status(Status.BAD_REQUEST), request);
 		}
 
-		DocumentContent doc = new DocumentContent(new DocumentKey(NullUUID.NULL.getUuid(), "SHORTLINKS"), link,
-				code);
+		DocumentContent doc = new DocumentContent(new DocumentKey(NullUUID.NULL.getUuid(), "SHORTLINKS"), link, code);
 		documentService.save(doc, Long.valueOf(ttl).intValue());
 
 		try {
-			return appendAllowOriginHeader(Response.ok(code), request);
+			return appendAllowOriginHeader(Response.ok(shortLink), request);
 		} catch (Exception ex) {
 			return appendAllowOriginHeader(Response.status(Status.INTERNAL_SERVER_ERROR), request);
 		}
 	}
 
 	@GET
-	@Path("/{short}")
-	public Response code(@PathParam("short") String shortlink, //
+	@Path("/{code}")
+	public Response validate(@PathParam("code") String code, //
 			@Context HttpServletRequest request) { //
-		
-		Stream<DocumentContent> docs = documentService.find(new DocumentMetadata(NullUUID.NULL.getUuid(), "SHORTLINKS", NullUUID.NULL.getUuid()), new QueryOptions(shortlink));
+
+		Stream<DocumentContent> docs = documentService.find(
+				new DocumentMetadata(NullUUID.NULL.getUuid(), "SHORTLINKS", NullUUID.NULL.getUuid()),
+				new QueryOptions(code));
 		List<DocumentContent> links = docs.collect(Collectors.toList());
 		if (links.isEmpty()) {
 			return create401Response(request);
 		}
-		
+
 		try {
 			return Response.temporaryRedirect(new URI(links.get(0).getContent())).build();
 		} catch (URISyntaxException e) {
 			logger.error("Unable to redirect request", e);
+			return create401Response(request);
+		}
+	}
+
+	@PUT
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Path("/sms/{number}")
+	public Response send(@PathParam("number") String number, //
+			String shortlink, //
+			@Context HttpServletRequest request) { //
+
+		try {
+			// extract code from short link
+			URI uri = new URI(shortlink);
+
+			Stream<DocumentContent> docs = documentService.find(
+					new DocumentMetadata(NullUUID.NULL.getUuid(), "SHORTLINKS", NullUUID.NULL.getUuid()),
+					new QueryOptions(uri.getPath()));
+			List<DocumentContent> links = docs.collect(Collectors.toList());
+			if (links.isEmpty()) {
+				return create401Response(request);
+			}
+
+			sender.send(number, shortlink, configurationService.getProperties());
+			return Response.ok().build();
+		} catch (Exception e) {
+			logger.error("Unable to send SMS request", e);
 			return create401Response(request);
 		}
 	}
@@ -149,5 +185,11 @@ public class ShortLinksEndpoint extends AbstractEndpoint {
 			return RandomStringUtils.random(VERIFICATION_LINK_LENGTH, true, true);
 		else
 			return RandomStringUtils.random(VERIFICATION_CODE_LENGTH, false, true);
+	}
+
+	private String getShortLink(String code, HttpServletRequest request) {
+		// SL links requires rewrite rule from https://xxx.com/sl/code to
+		// https://xxx.com/unifiedpus-server/rest/shortlinks/code
+		return request.getScheme() + "://" + request.getServerName() + "/sl/" + code;
 	}
 }
