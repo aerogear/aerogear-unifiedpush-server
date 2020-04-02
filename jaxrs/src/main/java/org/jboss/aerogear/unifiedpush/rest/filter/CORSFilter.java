3c914efdddfc8d9c700ae482a850916e8f5474d1
Summers Pittman <secondsun@gmail.com>
2020-04-02 14:43:03 -0400

    Adding cors filter

---
+++ jaxrs/src/main/java/org/jboss/aerogear/unifiedpush/rest/filter/CORSFilter.java
@@ -0,0 +1,29 @@
+package org.jboss.aerogear.unifiedpush.service.filter;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+/**
+ * This class provides CORS headers to all of the APIs.  Before 2020 we only used them for the client APIs and not the admin apis.
+ */
+@Provider
+public class CORSFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Credentials", "true");
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Headers",
+                "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add(
+                "Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+}
